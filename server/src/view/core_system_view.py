9c29205d8a423ee2493b938cd86dc0961bf827d6
김민관 <alsrhks2508@naver.com>
2024-08-16 05:43:53 +0900

    엔드포인트 수정
    4개의 get 과 2개의 post 로 구현

--- server/src/view/core_system_view.py
+++ server/src/view/core_system_view.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, Optional
 from fastapi import FastAPI
 from view.master_view import Master_View, RequestHeader
 from view.parsers import Head_Parser
-from controller import Core_Controller
+from controller import Home_Controller, Core_Controller
 
 class Core_Service_View(Master_View):
     def __init__(self, app:FastAPI, endpoint:str, database, head_parser:Head_Parser) -> None:
@@ -10,85 +10,120 @@ class Core_Service_View(Master_View):
         self.__app = app
         self._endpoint = endpoint
         self.__database = database
-        self.register_route(endpoint)
+        self.home_route(endpoint)
 
-    def register_route(self, endpoint:str):
+    def home_route(self, endpoint:str):
         @self.__app.get(endpoint+'/home')
         def home():
             return 'Hello, This is Root of Core-System Service'
         
-        @self.__app.post(endpoint+'/home/login')
-        def login(raw_request:dict):
-            request = LoginRequest(request=raw_request)
-            core_controller=Core_Controller()
-            model = core_controller.get_my_bias_league(database=self.__database,
-                                                             request=request)
+        # 홈화면에 배너 정보
+        @self.__app.get('/home/banner')
+        def get_banner():
+            home_controller=Home_Controller()
+            model = home_controller.get_banner_data(database=self.__database,)
             response = model.get_response_form_data(self._head_parser)
             return response
         
-        @self.__app.post(endpoint+'/home/daily')
-        def daily_request(raw_request:dict):
-            request = DailyRequest(request=raw_request)
-            core_controller=Core_Controller()
-            model = core_controller.requese_daily_check(database=self.__database,
-                                                             request=request)
-            response = model.get_response_form_data(self._head_parser)
-            return response
-        
-        @self.__app.get('/home/my_bias/{email}/{token}')
-        def get_my_bias(email:str, token:str):
-            request = {
-                'token' : token,
-                'email' : email
-            }
-            core_controller=Core_Controller()
-            model = core_controller.get_my_bias_league(database=self.__database,
+        # 홈 화면에 최애 정보
+        @self.__app.post('/home/my_bias')
+        def get_my_bias(raw_request:dict):
+            request = TokenRequest(request=raw_request)
+            home_controller=Home_Controller()
+            model = home_controller.get_my_bias_data(database=self.__database,
                                                              request=request)
             response = model.get_response_form_data(self._head_parser)
             return response
 
-        @self.__app.get('/home/show_all_leagues')
-        def show_all_leagues():
+        # 회원의 bias의 리그를 받아내는 앤드 포인트 (post)
+        @self.__app.post('/home/my_bias_league')
+        def my_bias_leagues(raw_request:dict):
+            print(raw_request)
+            request = MyLeagueRequest(request=raw_request)
             core_controller=Core_Controller()
-            model = core_controller.get_all_leagues(database=self.__database)
+            model = core_controller.get_my_bias_league(database=self.__database, request=request)
             response = model.get_response_form_data(self._head_parser)
             return response
         
-        @self.__app.get('/home/get_solo_leagues/{league_id}')
-        def get_solo_leagues(league_id:str):
-            request = league_id
-            core_controller=Core_Controller()
-            model = core_controller.get_solo_league(database=self.__database,
-                                                    request=request)
+        # 메인화면에 전체 리그 보기에서 띄워줄 리그 이름 보내줘야됨
+        # http://127.0.0.1:6000/home/league_data?league_type=solo
+        @self.__app.get('/home/league_data')
+        def show_leagues(league_type:Optional[str] = "solo"):
+            request = LeagueTypeRequest(league_type=league_type)
+            home_controller=Home_Controller()
+            model = home_controller.get_league_meta_data(database=self.__database, request=request)
             response = model.get_response_form_data(self._head_parser)
             return response
+
+        # 리그 정보를 받아내는 기본적인 함수 (get)
+        # 리그 정보를 받아내는 방법
+        # http://127.0.0.1:6000/home/show_league?league_name=시리우스
+        # http://127.0.0.1:6000/home/show_league?league_id=1001
+        # http://127.0.0.1:6000/home/show_league?league_name=시리우스&league_id=1001
+        @self.__app.get('/home/show_league')
+        def show_leagues(league_name:Optional[str] = None, league_id:Optional[str] = None):
+            request = LeagueRequest(league_name=league_name, league_id=league_id)
+            core_controller=Core_Controller()
+            model = core_controller.get_league(database=self.__database, request=request)
+            response = model.get_response_form_data(self._head_parser)
+            return response
+
+        # 최애를 검색하는 보편적인 함수
+        # 목적 : 나의 최애 선택하기, 홈화면의 최애 검색 기능
+        #"http://127.0.0.1:6000/home/search_bias?bias_name=김"  # bias 검색
+        @self.__app.get('/home/search_bias')
+        def show_leagues(bias_name:Optional[str] = None):
+            request = BiasSearchRequest(bias_name=bias_name)
+            home_controller=Home_Controller()
+            model = home_controller.search_bias(database=self.__database, request=request)
+            response = model.get_response_form_data(self._head_parser)
+            return response
+
+        # 나의 최애 선택하기 (취소하기는 없음.... 취소하지 마라)
+        @self.__app.post('/home/try_select_my_bias')
+        def try_select_my_bias(raw_request:dict):
+            request = BiasSelectRequest(request=raw_request)
+            home_controller=Home_Controller()
+            model = home_controller.select_bias(database=self.__database, request=request)
+            response = model.get_response_form_data(self._head_parser)
+            return response
+
+        #@self.__app.post(endpoint+'/home/login')
+        #def login(raw_request:dict):
+            #request = LoginRequest(request=raw_request)
+            #core_controller=Core_Controller()
+            #model = core_controller.get_my_bias_league(database=self.__database,
+                                                             #request=request)
+            #response = model.get_response_form_data(self._head_parser)
+            #return response
         
-        @self.__app.get('/home/get_group_leagues/{league_id}')
-        def get_group_leagues(league_id:str):
-            request = league_id
-            core_controller=Core_Controller()
-            model = core_controller.get_group_league(database=self.__database,
-                                                     request=request)
-            response = model.get_response_form_data(self._head_parser)
-            return response
+        #@self.__app.post(endpoint+'/home/daily')
+        #def daily_request(raw_request:dict):
+            #request = DailyRequest(request=raw_request)
+            #core_controller=Core_Controller()
+            #model = core_controller.requese_daily_check(database=self.__database,
+                                                             #request=request)
+            #response = model.get_response_form_data(self._head_parser)
+            #return response
+
+
+        #@self.__app.get(endpoint+'/{sample}')
+        #def sample_get(sample:str):
+            #request = sample
+            #core_controller=Core_Controller()
+            #model = core_controller.sample_func(database=self.__database,
+                                                             #request=request)
+            #response = model.get_response_form_data(self._head_parser)
+            #return response
         
-        @self.__app.get(endpoint+'/{sample}')
-        def sample_post(sample:str):
-            request = sample
-            core_controller=Core_Controller()
-            model = core_controller.sample_func(database=self.__database,
-                                                             request=request)
-            response = model.get_response_form_data(self._head_parser)
-            return response
-        
-        @self.__app.post(endpoint+'/post_sample')
-        def sample_post(raw_request:dict):
-            request = SampleRequest(request=raw_request)
-            core_controller=Core_Controller()
-            model = core_controller.sample_func(database=self.__database,
-                                                             request=request)
-            response = model.get_response_form_data(self._head_parser)
-            return response
+        #@self.__app.post(endpoint+'/post_sample')
+        #def sample_post(raw_request:dict):
+            #request = SampleRequest(request=raw_request)
+            #core_controller=Core_Controller()
+            #model = core_controller.sample_func(database=self.__database,
+                                                             #request=request)
+            #response = model.get_response_form_data(self._head_parser)
+            #return response
 
 class SampleRequest(RequestHeader):
     def __init__(self, request) -> None:
@@ -101,14 +136,46 @@ class LoginRequest(RequestHeader):
     def __init__(self, request) -> None:
         super().__init__(request)
         body = request['body']
-        self.email = body['email']
         self.token = body['token']
+
+class TokenRequest(RequestHeader):
+    def __init__(self, request) -> None:
+        super().__init__(request)
+        body = request['body']
+        self.token = body['token']
+
+class MyLeagueRequest(RequestHeader):
+    def __init__(self, request) -> None:
+        super().__init__(request)
+        body = request['body']
+        self.token = body['token']
+        self.league_type = body['type']
+
+class LeagueTypeRequest():
+    def __init__(self, league_type= None) -> None:
+        self.league_type = league_type
+
+class LeagueRequest():
+    def __init__(self, league_name = None, league_id = None) -> None:
+        self.league_name = league_name
+        self.league_id = league_id
+
+class BiasSearchRequest():
+    def __init__(self, bias_name= None) -> None:
+        self.bias_name =bias_name 
+
+
+class BiasSelectRequest(RequestHeader):
+    def __init__(self, request) -> None:
+        super().__init__(request)
+        body = request['body']
+        self.token = body['token']
+        self.bid = body['bid']
 
 class DailyRequest(RequestHeader):
     def __init__(self, request) -> None:
         super().__init__(request)
         body = request['body']
-        self.email = body['email']
         self.token = body['token']
 
 
