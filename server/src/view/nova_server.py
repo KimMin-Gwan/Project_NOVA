309bddd1130f4e262328b2fb9a33436a0bcce62a
김민관 <alsrhks2508@naver.com>
2024-08-17 01:16:38 +0900

    Update nova_server.py

--- server/src/view/nova_server.py
+++ server/src/view/nova_server.py
@@ -2,11 +2,25 @@ from fastapi import FastAPI
 from view.core_system_view import Core_Service_View
 from view.parsers import Head_Parser
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class NOVA_Server:
     def __init__(self, database) -> None:
         self.__app = FastAPI()
+
+        origins = [
+            "http://localhost.tiangolo.com",
+        ]
+        # 미들웨어 추가
+        self.__app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
         head_parser = Head_Parser()
         self.__core_system_view = Core_Service_View(app=self.__app,
                                                    endpoint='/core_system',
