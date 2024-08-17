b63676c46f864cb9e4de24abc0e71115858fa76c
Seungjun <121211597+Seungjun07@users.noreply.github.com>
2024-08-18 02:59:52 +0900

    수정4
    최애 사진 추가

--- nova_web/src/App.js
+++ nova_web/src/App.js
@@ -5,7 +5,13 @@ import Banner from './component/banner';
 import Modal from './component/modal';
 import MyPage from './pages/Mypage';
 import MyBias from './pages/MyBias';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
+import zoom from './img/zoom.png';
+import menu from './img/menu.png';
+import plus from './img/plus.png';
+import more from './img/more.png';
+
+// bid = ''가 이거면 오른쪽 레이아웃 바둑판
 
 function App() {
 
@@ -32,14 +38,18 @@ function App() {
     "uid": '1234-abcd-5678',
     "endpoint": "/core_system/",
   }
+
+  let jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RVc2VyQG5hdmVyLmNvbSIsImlhdCI6MTcyMzkxNjE2NywiZXhwIjoxNzIzOTE3OTY3fQ.NnAH7uEV85D-J_T_5dZeuwP9-pRj8j794lNW0sa4YJs'
+
   let send_data = {
     "header": header,
     "body": {
-      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RVc2VyQG5hdmVyLmNvbSIsImlhdCI6MTcyMzkwNjk1NiwiZXhwIjoxNzIzOTA4NzU2fQ.eBUmzqPCJnTB-3Fi29r13BrLvCeSr_bNsVh-idaS6O4'
+      'token': jwt
     }
   }
 
   let bias_url = url + 'my_bias';
+  let solo_bias = 'https://kr.object.ncloudstorage.com/cheese-images/';
   useEffect(() => {
     fetch(bias_url, {
       method: 'POST',
@@ -50,14 +60,15 @@ function App() {
     })
       .then(response => response.json())
       .then(data => {
-        bias_copy = data.body.solo_bias.bname;
+        bias_copy = data.body.solo_bias;
         setBias(bias_copy);
-        console.log(JSON.stringify(data.body.solo_bias.bname))
+        // console.log(JSON.stringify(data.body.solo_bias))
       })
   }, [bias_url])
 
   let [rank, setRank] = useState([]);
   let rank_copy = [];
+  let profile_url = 'https://kr.object.ncloudstorage.com/nova-images/';
 
   let rank_url = url + `show_league?league_name=${leagues}`;
   useEffect(() => {
@@ -73,6 +84,23 @@ function App() {
       })
   }, [rank_url])
 
+  // false 일경우 전체
+  let [my_or_all, setNow] = useState(false);
+  let [select, setSelect] = useState('select-not');
+
+  function selectChange() {
+    if (my_or_all === false) {
+      setNow('true');
+      setSelect('select-not');
+    }
+    if (my_or_all === true) {
+      setNow('false');
+      setSelect('select-now');
+    }
+  }
+
+
+
   return (
     <Routes>
       <Route path='/mypage' element={<MyPage />}></Route>
@@ -81,26 +109,56 @@ function App() {
         <div className="container">
           <div className="top-area">
             <header className='header'>
-              <div className='logo'>로고</div>
+              <div className='logo'>NOVA</div>
               <div className='buttons'>
-                <Link to='/' className='button'>홈</Link>
+                <button>
+                  <img src={zoom}></img>
+                </button>
+                <button>
+                  <img src={menu}></img>
+                </button>
+                {/* <Link to='/' className='button'>홈</Link>
                 <Link to='/mypage' className='button'>마이페이지</Link>
-                <Link to='/mybias' className='button'>최애페이지</Link>
+                <Link to='/mybias' className='button'>최애페이지</Link> */}
               </div>
             </header>
             <Banner></Banner>
+            <h2>인증하기</h2>
             <section className='my-bias'>
-              <div className='left-box'>name={bias}</div>
-              <div className='left-box'>11</div>
+              <div className='left-box'>
+                <img src={solo_bias + `${bias.bid}.jpg`}></img>
+                <div className='support'>지지하기</div>
+                <button className='more'>
+                  <img src={more}></img>
+                </button>
+              </div>
+              <div className='left-box'>
+                <img src={solo_bias + `${bias.bid}.jpg`}></img>
+                <div className='support'>지지하기</div>
+                <button className='more'>
+                  <img src={more}></img>
+                </button>
+              </div>
             </section>
           </div>
+
+          {/* <div class="toggle-container">
+            <input type="radio" id="option1" name="toggle" checked />
+            <label for="option1" class="toggle-label">내 최애</label>
+
+            <input type="radio" id="option2" name="toggle" />
+            <label for="option2" class="toggle-label">전체</label>
+
+            <div class="toggle-slider"></div>
+          </div> */}
+
 
           <section className="solo-bias-rank">
             <div className='title-area'>
               <div className="ranking">개인리그 랭킹</div>
-              <div className='select'>
-                <div className='bias'>내 최애</div>
-                <div className='all-bias'>전체</div>
+              <div className='select-ranking'>
+                <div onClick={selectChange} className={select}>내 최애</div>
+                <div onClick={selectChange} className={select}>전체</div>
               </div>
             </div>
             <div className="stars">
@@ -114,15 +172,19 @@ function App() {
               <button>8</button>
               <button>9</button>
               <button>10</button>
+              {/* 눌리면 색 바뀌게
+              버튼마다 내용다르게
+                    */}
             </div>
-            <h3>00리그</h3>
             <div className="league">
               {
                 rank.map(function (a, i) {
                   return (
                     <div className="rank-item-box" key={i}>
-                      <div>{i+1}.</div>
-                      <div className="rank-profile"></div>
+                      <div className='number'>{i + 1}.</div>
+                      <div className="rank-profile">
+                        <img src={profile_url + `${rank[i].bid}.PNG`}></img>
+                      </div>
                       <div className="name">{rank[i].bname}</div>
                       <div className="point">1000pt</div>
                     </div>
--- nova_web/src/component/banner.js
+++ nova_web/src/component/banner.js
@@ -1,6 +1,3 @@
-import img1 from '../img/img1.jpg';
-import img2 from '../img/img2.jpg';
-import img3 from '../img/img3.jpeg';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
