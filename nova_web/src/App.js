eea67e0bdf3a95bd9626a5b7b6b2657e86e165f6
Seungjun <121211597+Seungjun07@users.noreply.github.com>
2024-08-18 00:26:59 +0900

    수정 3

--- nova_web/src/App.js
+++ nova_web/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Banner from './component/banner';
 import Modal from './component/modal';
 import MyPage from './pages/Mypage';
@@ -8,6 +8,70 @@ import MyBias from './pages/MyBias';
 import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
+
+  let [leagues, setLeague] = useState([]);
+  let league_copy = [];
+  let url = 'http://127.0.0.1:80/home/';
+
+  useEffect(() => {
+    fetch(url + 'league_data?league_type=solo')
+      .then(response => response.json())
+      .then(data => {
+        // league_copy = data.body.leagues[0].lname
+        league_copy = data.body.leagues.map(leagues => leagues.lname);
+        setLeague(league_copy);
+      })
+  }, [url])
+
+  let [bias, setBias] = useState([]);
+  let bias_copy = [];
+  let header = {
+    "request-type": "default",
+    "client-version": 'v1.0.1',
+    "client-ip": '127.0.0.1',
+    "uid": '1234-abcd-5678',
+    "endpoint": "/core_system/",
+  }
+  let send_data = {
+    "header": header,
+    "body": {
+      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RVc2VyQG5hdmVyLmNvbSIsImlhdCI6MTcyMzkwNjk1NiwiZXhwIjoxNzIzOTA4NzU2fQ.eBUmzqPCJnTB-3Fi29r13BrLvCeSr_bNsVh-idaS6O4'
+    }
+  }
+
+  let bias_url = url + 'my_bias';
+  useEffect(() => {
+    fetch(bias_url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify(send_data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        bias_copy = data.body.solo_bias.bname;
+        setBias(bias_copy);
+        console.log(JSON.stringify(data.body.solo_bias.bname))
+      })
+  }, [bias_url])
+
+  let [rank, setRank] = useState([]);
+  let rank_copy = [];
+
+  let rank_url = url + `show_league?league_name=${leagues}`;
+  useEffect(() => {
+
+    fetch(rank_url)
+      .then(response => response.json())
+      .then(data => {
+        // rank_copy = [...data.body.rank].map()
+        rank_copy = [...data.body.rank];
+        setRank(rank_copy);
+        // console.log(rank_copy.bname);
+        // console.log(JSON.stringify([...rank_copy[0]][0].bname))
+      })
+  }, [rank_url])
 
   return (
     <Routes>
@@ -24,15 +88,9 @@ function App() {
                 <Link to='/mybias' className='button'>최애페이지</Link>
               </div>
             </header>
-            {/* 
-        1. 시간이 지나면 자동으로 다음 베너로 넘어가도록
-        2. 클릭 시 다음 베너로 넘어가도록
-        3. 클릭 전 이미지 */}
             <Banner></Banner>
-
-
             <section className='my-bias'>
-              <div className='left-box'>11</div>
+              <div className='left-box'>name={bias}</div>
               <div className='left-box'>11</div>
             </section>
           </div>
@@ -42,17 +100,11 @@ function App() {
               <div className="ranking">개인리그 랭킹</div>
               <div className='select'>
                 <div className='bias'>내 최애</div>
-                <div onClick={() => {
-                  fetch('http://127.0.0.1:80/home/league_data?league_type=solo')
-                    .then(response => response.json())
-                    .then(data => {
-                      console.log(JSON.stringify(data));
-                    })
-                }} className='all-bias'>전체</div>
+                <div className='all-bias'>전체</div>
               </div>
             </div>
             <div className="stars">
-              <button>오리온자리</button>
+              <button>{leagues}</button>
               <button>사수자리</button>
               <button>천칭자리</button>
               <button>물병자리</button>
@@ -65,18 +117,18 @@ function App() {
             </div>
             <h3>00리그</h3>
             <div className="league">
-              <div className="rank-item-box">
-                <div>1.</div>
-                <div className="rank-profile"></div>
-                <div className="name">이름</div>
-                <div className="point">1000pt</div>
-              </div>
-              <div className="rank-item-box">
-                <div>1.</div>
-                <div className="rank-profile"></div>
-                <div className="name">이름</div>
-                <div className="point">1000pt</div>
-              </div>
+              {
+                rank.map(function (a, i) {
+                  return (
+                    <div className="rank-item-box" key={i}>
+                      <div>{i+1}.</div>
+                      <div className="rank-profile"></div>
+                      <div className="name">{rank[i].bname}</div>
+                      <div className="point">1000pt</div>
+                    </div>
+                  )
+                })
+              }
             </div>
           </section>
 
--- nova_web/src/component/banner.js
+++ nova_web/src/component/banner.js
@@ -5,58 +5,57 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Banner() {
-  let [currentBanner, setBanner] = useState(1);
+  let [currentBanner, setBanner] = useState(0);
   let [images, setImage] = useState([]);
   // 
   let url = 'http://127.0.0.1:80/home/banner';
   // let data = { token: '토큰 정보'}
-  // useEffect(() => {
-  //   // let a = setTimeout(()=>{ setBanner(currentBanner=1)}, 2000)
-  //   return () => {
-  //     // clearTimeout(a);
-  //     // setBanner(currentBanner=0);
-  //   }
-  // // }, [])
-  // function getImages(){
-  //   return ()
-  // }
 
   useEffect(() => {
+    let copy = [];
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        let copy = [...data.body.banner];
-        setImage(copy[0].ba_url);
-        // console.log(copy);
-        // console.log(JSON.stringify(copy[0].ba_url));
-        // setImage(copy[1].ba_url);
-        // console.log(JSON.stringify(images));
-        // console.log(images)
+        copy = data.body.banner.map(banner => banner.ba_url);
+        // copy = [data.body.banner[0].ba_url, data.body.banner[1].ba_url];
+        setImage(copy);
       });
-    // let a = setTimeout(()=>{ setImage(0)},2000)
+    
+      let a = setInterval(()=>{ 
+      setBanner((prevIndex)=>{
+        const nextIndex = (prevIndex + 1) % copy.length;
+        return nextIndex;
+      })}, 3000)
+    
+    //   copy.map(function(b, i){
+    //   if(copy.length>i)
+    //   {
+    //     setImage([copy[0]]);
+    //   }
+    // })
 
     return () => {
-      // clearTimeout(a);
-    }
-  }, [])
+      clearInterval(a);
+      // setImage([copy[0]]);
+    };
+  }, [url])
 
   return (
     <section className="banner">
       <div className="banner-images">
-        {/* {
+        {
           images.map(function (a, i) {
             return (
               <div className="image-box" key={i}>
-                <div>{images}</div>
-                <img className="image1" src={images}></img>
+                <img className="image1" src={images[currentBanner]}></img>
               </div>
             )
           })
-        } */}
+        }
 
-        <div className="image-box">
-          <img className="image1" src={images}></img>
-        </div>
+        {/* <div className="image-box">
+          <img className="image1" src={images[currentBanner]}></img>
+        </div> */}
         {/* <div className="image-box">
           <img className="image2" src={url}></img>
         </div> */}
