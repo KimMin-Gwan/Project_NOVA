17501c2db6678036e9a56510245498b98018cb66
김민관 <alsrhks2508@naver.com>
2024-08-18 20:10:16 +0900

    Create instance_test.py

---
+++ server/test/instance_test.py
@@ -0,0 +1,17 @@
+
+class Item():
+    def __init__(self, init_data = {"chatting" : 0, "saver":0}):
+        self.chatting  = init_data["chatting"]
+        self.saver = init_data["saver"]
+
+    def get_dict_form_data(self):
+        return {
+            "chatting" : self.chatting,
+            "saver": self.saver
+        }
+
+
+
+item = Item(init_data={"chatting": 0, "saver" : 1})
+
+print(item.saver)
