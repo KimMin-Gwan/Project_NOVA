7961be759abaf718e424690bb13f79078d8b1125
Vespoi <103725786+Vespoi@users.noreply.github.com>
2024-08-19 23:47:47 +0900

    로그인, 회원가입 테스트

--- server/src/controller/user_controller.py
+++ server/src/controller/user_controller.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-class UserController:
+class UserController:      
     def sample_func(self, database:Local_Database, request) -> BaseModel: 
         #jwt_decoder = JWTDecoder()
         
@@ -38,7 +38,6 @@ class UserController:
 
         finally:
             return model
-        
     # 작성요령
     # - Model들은 대충 다른 모델들 복붙해서 쓸것
     # - Model들은 model/user_models.py 안에 모두 작성할것 (3개 다)
@@ -49,10 +48,36 @@ class UserController:
         
     # 로그인 시도
     # 1. 데이터베이스에서 검증
-    def try_lgoin(self, database, request):
-        #model = LoginModel()
-        #return model
-        pass # 작성 끝나면 지울것
+    def try_login(self, database, request):
+
+        model = LoginModel(database=database)
+        try:
+            # 유저가 있는지 확인
+            if not model.set_user_with_email(request=request):
+                raise UserNotExist("Can not find User with uid")
+        except UserNotExist as e:
+            print("Error Catched : ", e)
+            model.set_state_code(e.error_code) # 종합 에러
+            return model
+
+        try:
+            model.request_login(request=request,user_data=model._user)
+
+            if not model.get_result:
+                return model
+            
+            model.make_token(request=model._user)
+
+        except CustomError as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        except Exception as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        finally:
+            return model
 
     # 이메일 인증
     # 1. 데이터 베이스에 이미 있는 email인지 확인
@@ -63,9 +88,26 @@ class UserController:
     # 6. 이미 있는 email이면 False 반환
 
     def try_send_email(self, database, request, nova_verification):
-        #model = SendEmailModel()
-        #return model
-        pass # 작성 끝나면 지울것
+        model = SendEmailModel(database=database)
+        try:
+            if not model.set_user_with_email(request=request):
+                mailsender = MailSender()
+                temp_user = nova_verification.make_new_user(email=request.email)
+
+                mailsender.send_email(receiver_email=temp_user.email,verification_code=temp_user.verification_code)
+            else:
+                model.set_response()
+
+        except CustomError as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        except Exception as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        finally:
+            return model
 
     # 회원가입 시도
     # 1. 인증번호 맞는지 확인
@@ -74,10 +116,24 @@ class UserController:
     # 4. 다 되면 True반환
     # 5. 만약 인증번호 틀리면 False 반환 + 실패 사유 detail에 작성
     def try_sign_in(self, database, request, nova_verification):
-        #model = SendEmailModel()
+        model = SendEmailModel(database=database)
+        try:
+            if not nova_verification.verificate_user(email=request.email, verification_code=request.verification_code):
+                model.set_response()
+            else:
+                model.save_user(request=request)
+                model.make_token(request=request)
 
-        #return model
-        pass # 작성 끝나면 지울것
+        except CustomError as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        except Exception as e:
+            print("Error Catched : ", e.error_type)
+            model.set_state_code(e.error_code) # 종합 에러
+
+        finally:
+            return model
 
 
 # 이메일 전송
--- server/src/model/user_models.py
+++ server/src/model/user_models.py
@@ -1,25 +1,56 @@
 from model.base_model import BaseModel
 from model import Local_Database
-from others.data_domain import Bias, League
+from others.data_domain import User
 from others import CoreControllerLogicError
+import jwt
+import datetime
+import string
+import random
 
-class SampleModel(BaseModel):
+class LoginModel(BaseModel):
     def __init__(self, database:Local_Database) -> None:
         super().__init__(database)
         self.__result = False
+        self.__token = ''
+        self.__detail = ''
 
-    def set_function(self):
+    def request_login(self,request,user_data):
         try:
-            data = self._database.get_all_data(target="banner")
-            self.__result = True 
+            if request.email == user_data.email and request.password == user_data.password:
+                self.__result = True
+            elif request.password != user_data.password:
+                self.__detail = 'worng password'
 
         except Exception as e:
-            raise CoreControllerLogicError(error_type="set_group_leagues | " + str(e))
+            raise CoreControllerLogicError(error_type="request_login | " + str(e))
+    
+    def make_token(self,request):
+        try:
+            # 비밀 키 설정
+            secret_key = "your_secret_key"
+            # 헤더 설정
+            headers = {
+                "alg": "HS256",
+                "typ": "JWT"
+            }
+            # 페이로드 설정
+            payload = {
+                "email": request.email,
+                "iat": datetime.datetime.utcnow(),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)  # 만료 시간 30분
+            }
+            # 토큰 생성
+            self.__token = jwt.encode(payload, secret_key, algorithm="HS256", headers=headers) 
+
+        except Exception as e:
+            raise CoreControllerLogicError(error_type="login make_token | " + str(e))
 
     def get_response_form_data(self, head_parser):
         try:
             body = {
-                'result ': self.__result
+                'resust' : self.__result,
+                'detail' : self.__detail,
+                'token' : self.__token
             }
 
             response = self._get_response_data(head_parser=head_parser, body=body)
@@ -27,3 +58,74 @@ class SampleModel(BaseModel):
 
         except Exception as e:
             raise CoreControllerLogicError("response making error | " + e)
+    
+    def get_result(self):
+        return self.__result
+    
+
+class SendEmailModel(BaseModel):
+    def __init__(self, database:Local_Database) -> None:
+        super().__init__(database)
+        self.__result = True
+        self.__detail = ''
+        self.__token = ''
+
+    def make_token(self,request):
+        try:
+            # 비밀 키 설정
+            secret_key = "your_secret_key"
+            # 헤더 설정
+            headers = {
+                "alg": "HS256",
+                "typ": "JWT"
+            }
+            # 페이로드 설정
+            payload = {
+                "email": request.email,
+                "iat": datetime.datetime.utcnow(),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)  # 만료 시간 30분
+            }
+            # 토큰 생성
+            self.__token = jwt.encode(payload, secret_key, algorithm="HS256", headers=headers)
+
+        except Exception as e:
+            raise CoreControllerLogicError(error_type="make_token | " + str(e))
+        
+    def save_user(self,request):
+        try:
+            letters_set = string.ascii_lowercase + string.digits
+            letters =[]
+            for _ in range(3):
+                random_list = random.sample(letters_set,4)
+                result = ''.join(random_list)
+                letters.append(result)
+
+            uid = f'{letters[0]}-{letters[1]}-{letters[2]}'
+            user = User(uid=id,age=request.age,email=request.email,gender=request.gender,password=request.password)
+
+            self._database.add_new_data(target_id='uid',new_data=user.get_dict_form_data())
+        except Exception as e:
+            raise CoreControllerLogicError(error_type="set_response | " + str(e))
+        
+    def set_response(self,):
+        try:
+            self.__result = False
+        except Exception as e:
+            raise CoreControllerLogicError(error_type="set_response | " + str(e))
+
+    def get_response_form_data(self, head_parser):
+        try:
+            body = {
+                'resust' : self.__result,
+                'token' : self.__token,
+                'deyail' : self.__detail
+            }
+
+            response = self._get_response_data(head_parser=head_parser, body=body)
+            return response
+
+        except Exception as e:
+            raise CoreControllerLogicError("response making error | " + e)
+    
+    def get_result(self):
+        return self.__result
