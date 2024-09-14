f23ef6342289a39ac0d5e10081474a1073757018
김민관 <alsrhks2508@naver.com>
2024-09-15 03:18:22 +0900

    Update nova_server.py

--- server/src/view/nova_server.py
+++ server/src/view/nova_server.py
@@ -20,7 +20,7 @@ class NOVA_Server:
         self.__app.add_middleware(
             CORSMiddleware,
             allow_origins=[
-                "*"],  # 원격 호스트의 웹소켓 연결을 허용할 도메인 설정
+                "http://localhost:3000"],  # 원격 호스트의 웹소켓 연결을 허용할 도메인 설정
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
