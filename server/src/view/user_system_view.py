bcd71bd6651c6aa786f0b7aaa1a26e9b54a84048
김민관 <alsrhks2508@naver.com>
2024-09-27 02:37:54 +0900

    Update user_system_view.py

--- server/src/view/user_system_view.py
+++ server/src/view/user_system_view.py
@@ -72,7 +72,7 @@ class User_Service_View(Master_View):
 
     def my_page_route(self):
         # feed 랑 상호작용 -> 댓글 좋아요 하기
-        @self.__app.get('/user_home/get_my_data')
+        @self.__app.get('/user_home/my_data')
         def get_my_page(request:Request):
             request_manager = RequestManager()
 
