c728c6f3d8c7c3cfb23502cec428d211d4d813b8
betteree <kkne1017@gmail.com>
2024-10-06 03:21:37 +0900

    공지사항 페이지 1차

--- nova_web/src/pages/Notice/Notice.js
+++ nova_web/src/pages/Notice/Notice.js
@@ -1,16 +1,36 @@
-import { Link } from 'react-router-dom';
-import style from './NoticeList.module.css';
-
+import { Link, useNavigate } from "react-router-dom";
+import style from "./NoticeList.module.css";
+import backword from "./../../img/back_icon.png";
 function Notice() {
-    return (
-        <div className={style['main-body']}>
-            <div className={style['top-area']}>공지사항</div>
-            <div className={style['notice-area']}>
-                <div className={style.notice}>제목</div>
-                <div className={style['png-area']}>공지사항 (png)</div>
-            </div>
+  let navigate = useNavigate();
+  return (
+    <div className={style["main-body"]}>
+      <div className={style.TopBar}>
+        <img
+          src={backword}
+          alt="Arrow"
+          className={style.backword}
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
+        <div className={style.TitleBox}>
+          <p className={style.titleName}> 공지사항 </p>
         </div>
-    )
+        <div className={style.EmptyBox} />
+      </div>
+
+      <div className={style["notice-area"]}>
+        <div className={style.notice}>
+          <p className={style.titleName}>제목 </p>
+          <p className={style.date}>2024/10/7</p>
+        </div>
+        <div className={style["png-area"]}>
+          <p className={style.text}>본문 예시입니다. </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Notice;
--- nova_web/src/pages/Notice/NoticeList.js
+++ nova_web/src/pages/Notice/NoticeList.js
@@ -1,20 +1,55 @@
-import { Link, useNavigate } from 'react-router-dom';
-import style from './NoticeList.module.css';
+import { Link, useNavigate } from "react-router-dom";
+import backword from "./../../img/back_icon.png";
+import style from "./NoticeList.module.css";
 
-function NoticeList(){
+function NoticeList() {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    return (
-        <div className={style['main-body']}>
-            <div className={style['top-area']}>공지사항</div>
-            <div className={style['notice-area']}>
-                <div className={style.notice} onClick={()=>{navigate('/notice')}}>공지사항 1</div>
-                <div className={style.notice} onClick={()=>{navigate('/notice')}}>공지사항 2</div>
-                <div className={style.notice} onClick={()=>{navigate('/notice')}}>공지사항 3</div>
-            </div>
+  return (
+    <div className={style["main-body"]}>
+      <div className={style.TopBar}>
+        <img
+          src={backword}
+          alt="Arrow"
+          className={style.backword}
+          onClick={() => {
+            navigate(-1);
+          }}
+        />
+        <div className={style.TitleBox}>
+          <p className={style.titleName}> 공지사항 </p>
         </div>
-    )
+        <div className={style.EmptyBox} />
+      </div>
+
+      <div className={style["notice-area"]}>
+        <div
+          className={style.noticelist}
+          onClick={() => {
+            navigate("/notice");
+          }}
+        >
+          <span className={style.noticeText}>공지사항 1</span>
+        </div>
+        <div
+          className={style.noticelist}
+          onClick={() => {
+            navigate("/notice");
+          }}
+        >
+          <span className={style.noticeText}>공지사항 2</span>
+        </div>
+        <div
+          className={style.noticelist}
+          onClick={() => {
+            navigate("/notice");
+          }}
+        >
+          <span className={style.noticeText}>공지사항 3</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NoticeList;
