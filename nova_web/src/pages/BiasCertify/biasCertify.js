073fb6f7ce38740c9bdf7b804fe4f25f994dfa8a
Seungjun <121211597+Seungjun07@users.noreply.github.com>
2024-10-06 18:12:27 +0900

    바이어스 인증페이지

--- nova_web/src/pages/BiasCertify/biasCertify.js
+++ nova_web/src/pages/BiasCertify/biasCertify.js
@@ -4,7 +4,11 @@ import bias from './../../img/bias.png';
 import bias_back from './../../img/bias_back.png';
 import circle from './../../img/circle.png';
 import ProgressBar from './ProgressBar';
+import star from '../../img/star.png';
+
 import { useEffect, useState } from 'react';
+import backword from "./../../img/back_icon.png";
+
 
 function BiasCertify() {
 
@@ -19,6 +23,11 @@ function BiasCertify() {
 
     // let url = 'https://nova-platform.kr/';
     let [biasData, setBiasData] = useState();
+    let [showNameCard, setShowNameCard] = useState(false);
+
+    function handleNameCard() {
+        setShowNameCard(true);
+    };
 
     let header = {
         "request-type": "default",
@@ -89,8 +98,9 @@ function BiasCertify() {
             })
                 .then(response => response.json())
                 .then(data => {
+                    setBiasData(data.body)
+                    // setBiasData((prevData) => ({ ...prevData, result: data.result }))
                     console.log('스페셜인증', data)
-                    setBiasData((prevData) => ({ ...prevData, result: data.result }))
                 });
         } else {
             alert('특별인증 시간이 아닙니다.');
@@ -109,13 +119,18 @@ function BiasCertify() {
     }
 
 
-    // if (biasData.result === 'daily done') {
+    // if (biasData.result === 'valid') {
     return (
         <div className={`container ${style['main-container']}`}>
             <div className={`white-title ${style['title-area']}`}>
-                <button onClick={() => {
-                    navigate(-1)
-                }}>뒤로</button>
+                <img
+                    src={backword}
+                    alt="Arrow"
+                    className={style.backword}
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                />
                 <div>나의 최애 지지하기</div>
             </div>
             <div className={style['certify-container']}>
@@ -125,179 +140,73 @@ function BiasCertify() {
                             <div>누적 적립 포인트 : </div>
                             <div>{biasData.point}</div>
                         </div>
-                        <ProgressBar />
+                        <ProgressBar point={biasData.contribution} />
                     </div>
                 </div>
                 <div className={style.area}>
-                    <div className={style['bias-area']}>
-                        <div className={style['name-area']}>이름영역 : {biasData.bias.bname}</div>
-                        <div className={style['img-area']}>
-                            <div className={style.circle}>
-                                <img src={circle}></img>
-                                {/* <div>
-                                    <img src={`https://kr.object.ncloudstorage.com/nova-images/${biasData.bias.bid}.PNG`}/>
-                                </div> */}
+                    {
+                        biasData.result === 'special done' || showNameCard ? (
+                            <div className={style['bias_card']}>
+                                <img src={biasData.name_card_url} alt='bias_img'/>
+                                {/* <div>bu</div> */}
                             </div>
-                        </div>
-                        <div className={style['bottom-area']}>
-                            <div className={style['text-area']}>최애 인증하고 '{biasData.bias.nickname.length == 0 ? biasData.bias.bname : biasData.bias.nickname[0]}'을(를) 응원해요!</div>
-                            <div className={style['button-area']}>
-                                {
-                                    biasData.result === 'valid' &&
-                                    <div className={style.buttons} onClick={() => {
-                                        fetchAuthenData()
-                                    }}>인증 하기</div>
-                                }
-                                {
-                                    biasData.result === 'daily done' &&
-                                    (
-                                        <>
-                                            <div className={style.buttons}
-                                                onClick={fetchSpecialAuthenData}
-                                            >특별 인증</div>
-                                            <div className={style.buttons}>명함 보기</div>
-                                        </>
-                                    )
-                                }
-                                {
-                                    biasData.result === 'special done' && <div>21</div>
-                                }
-                                {
-                                    // biasData.special_check_valid가 false면 alert창 잘못된 시간에 누르면
-                                }
-                                {/* {
-                                    biasData.result === 'invalid' && alert('잘못된 접근')
-                                } */}
-                                {/* {
-                                    result === 'invalid' || result === 'error' && alert('잘못된 접근입니다.')
-                                } */}
-                                {/* <div className={style.buttons}>명함 보기</div> */}
+                        ) : (
+                            <div className={style['bias-area']}>
+                                <div className={style['name-area']}>
+                                    <div className={`star ${style['star_img']}`}>
+                                        <img src={star} />
+                                    </div>
+                                    <div className={style['name_style']}>
+                                        {biasData.bias.bname}
+                                    </div>
+                                    <div className={`star ${style['star_img']}`}>
+                                        <img src={star} />
+                                    </div>
+                                </div>
+                                <div className={style['img-area']}>
+                                    <div className={style.circle}>
+                                        <img src={circle} alt='circle'></img>
+                                        <div className={style['bias_img']}>
+                                            <img src={`https://kr.object.ncloudstorage.com/nova-images/${biasData.bias.bid}.PNG`} alt='bias_img'/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={style['bottom-area']}>
+                                    <div className={style['text-area']}>최애 인증하고 '{biasData.bias.nickname.length == 0 ? biasData.bias.bname : biasData.bias.nickname[0]}'을(를) 응원해요!</div>
+                                    <div className={style['button-area']}>
+                                        {
+                                            biasData.result === 'valid' &&
+                                            <div className={style.buttons} onClick={() => {
+                                                fetchAuthenData()
+                                            }}>인증 하기</div>
+                                        }
+                                        {
+                                            biasData.result === 'daily done' &&
+                                            (
+                                                <>
+                                                    <div className={style.buttons}
+                                                        onClick={fetchSpecialAuthenData}
+                                                    >특별 인증</div>
+                                                    <div className={style.buttons} onClick={handleNameCard}>명함 보기</div>
+                                                </>
+                                            )
+                                        }
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        {/* <img className={style.back} src={bias_back}></img> */}
-                        {/* <div>이름</div> */}
-                        {/* <div className={style.circle}>
-                            <img src={circle}></img>
-                        </div> */}
-                        {/* <div className={style['bottom-area']}>
-                            <div>응원해요</div>
-                        </div> */}
-                    </div>
+                        )
+                    }
                 </div>
             </div>
+            {
+                 biasData.result === 'special done' || showNameCard && (
+                    <div className={style['download-btn-area']}>
+                        <button>다운로드</button>
+                    </div>
+                 )
+            }
             <div className={style.advise}>광고 영역</div>
         </div>
     )
-    // }
-    //     else if (checkResult === 'daily done') {
-    //         return (
-    //             <div className={`container ${style['main-container']}`}>
-    //                 <div className={`white-title ${style['title-area']}`}>
-    //                     <button onClick={() => {
-    //                         navigate(-1)
-    //                     }}>뒤로</button>
-    //                     <div>나의 최애 지지하기</div>
-    //                 </div>
-    //                 <div className={style['certify-container']}>
-    //                     <div className={style['point-area']}>
-    //                         <div className={style['save-point-box']}>
-    //                             <div className={style.point}>
-    //                                 <div>누적 적립 포인트 : {point}</div>
-    //                                 <div>80%</div>
-    //                             </div>
-    //                             <ProgressBar />
-    //                             {/* <div className={style['progress-container']}>
-    //                             <div className={style['progress-bar']}></div>
-    //                         </div> */}
-    //                             {/* <progress value={50} min={0} max={100}>22</progress> */}
-    //                         </div>
-    //                     </div>
-    //                     <div className={style.area}>
-    //                         <div className={style['bias-area']}>
-    //                             <div className={style['name-area']}>이름영역 : {bias.bname}</div>
-    //                             <div className={style['img-area']}>
-    //                                 <div className={style.circle}>
-    //                                     <img src={circle}></img>
-    //                                     {/* <div>
-    //                                     <img src={`https://kr.object.ncloudstorage.com/nova-images/${bias.bid}.PNG`}/>
-    //                                 </div> */}
-    //                                 </div>
-    //                             </div>
-    //                             <div className={style['bottom-area']}>
-    //                                 <div className={style['text-area']}>최애 인증하고 '{bias.nickname === '' ? bias.bname : bias.nickname[0]}'을(를) 응원해요!</div>
-    //                                 <div className={style['button-area']}>
-    //                                     {/* {
-    //                                         result === 'valid' && <div className={style.buttons} onClick={() => {
-    //                                             fetch(url, {
-    //                                                 method: 'post',
-    //                                                 headers: {
-    //                                                     "Content-Type": 'application/json',
-    //                                                 },
-    //                                                 body: JSON.stringify(send_data),
-    //                                             })
-    //                                                 .then(response => response.json())
-    //                                                 .then(data => {
-    //                                                     // JSON.stringify(data)
-    //                                                     console.log(data.body)
-    //                                                     setCheckResult(data.body.result);
-    //                                                 });
-    //                                         }}>인증 하기</div>
-    //                                     } */}
-    //                                     {
-    //                                         checkSpecialTime !== 'special done' &&
-    //                                         <div className={style.buttons} onClick={() => {
-    //                                             specialTime.includes(currentTime) ? alert('특별 인증이 완료되었습니다.')
-    //                                                 : alert('특별시가 아닙니다.')
-    //                                             fetch(url + 'try_special_check', {
-    //                                                 method: 'post',
-    //                                                 headers: {
-    //                                                     "Content-Type": 'application/json',
-    //                                                 },
-    //                                                 body: JSON.stringify(send_data),
-    //                                             })
-    //                                                 .then(response => response.json())
-    //                                                 .then(data => {
-    //                                                     // JSON.stringify(data)
-    //                                                     console.log(data)
-    //                                                     setSpecialTime(data.body.result);
-    //                                                     console.log(specialTime)
-    //                                                 });
-
-    //                                         }}>특별 인증</div>
-    //                                         //time imvalid alert창 지금은 시간이 아님
-    //                                     }
-    //                                     {/* {
-    //                                         specialTime.includes(currentTime) ? alert('특별 인증이 완료되었습니다.')
-    //                                             : alert('특별시가 아닙니다.')
-    //                                     } */}
-    //                                     {
-    //                                         result === 'invalid' || result === 'error' && alert('잘못된 접근입니다.')
-    //                                     }
-    //                                     <div className={style.buttons}>명함 보기</div>
-    //                                 </div>
-    //                             </div>
-    //                             {/* <img className={style.back} src={bias_back}></img> */}
-    //                             {/* <div>이름</div> */}
-    //                             {/* <div className={style.circle}>
-    //                             <img src={circle}></img>
-    //                         </div> */}
-    //                             {/* <div className={style['bottom-area']}>
-    //                             <div>응원해요</div>
-    //                         </div> */}
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //                 <div className={style.advise}>광고 영역</div>
-    //             </div>
-    //         )
-    //     }
-    //     else if (bias === undefined) {
-    //         return (<div>loading</div>)
-    //     }
-    //     else {
-    //         return (<div>ddd</div>
-    //         )
-    //     }
-    // }
 }
 export default BiasCertify;
