9b49dc2400e628c81a83c0a6729c1ff7276ed1d9
betteree <kkne1017@gmail.com>
2024-10-07 04:08:41 +0900

    마이페이지 css

--- nova_web/src/pages/MyPage/Mypage.js
+++ nova_web/src/pages/MyPage/Mypage.js
@@ -2,7 +2,13 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import style from "./Mypage.module.css";
 import backword from "./../../img/back_icon.png";
 import ticket_icon from "./../../img/ticket_icon.png";
+import funding_icon from "./../../img/funding_icon.png";
+import novaAL_icon from "./../../img/novaAL_icon.png";
+import judge_icon from "./../../img/judge_icon.png";
+import galaxyleague from "./../../img/galaxyleague.png";
+import star_powder from "./../../img/starpowder.png";
 import { TfiCommentAlt } from "react-icons/tfi";
+
 import { CiStar, CiEdit } from "react-icons/ci";
 // import { SlSpeech } from "react-icons/sl";
 import { HiOutlineBellAlert } from "react-icons/hi2";
@@ -46,6 +52,28 @@ function MyPage() {
     navigate(page);
   }
 
+  const handleLogout = (e) => {
+    e.preventDefault();
+    fetch("https://nova-platform.kr/user_home/try_logout", {
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.text().then((text) => {
+            throw new Error(`Error: ${response.status}, ${text}`);
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Logout error:", error);
+      });
+  };
+
   return (
     <div className={style.container}>
       <div className={style.top_area}>
@@ -64,14 +92,16 @@ function MyPage() {
           <div className={style["sup_part"]}>
             <div className={style["wrap_sup"]}>
               <div className={style.support}>지지자</div>
-              <div className={style["space_pass"]}>우주인 패스 +</div>
-              <div onClick={handleClick}>{isClicked ? <IoIosArrowBack /> : <IoIosArrowForward />}</div>
+              <div className={style.space_pass}>우주인 패스 +</div>
+              <div onClick={handleClick}>{isClicked ? <IoIosArrowBack size={40} /> : <IoIosArrowForward size={40} />}</div>
             </div>
           </div>
           <div className={style["ticket_part"]}>
             <div className={style["ticket_info"]}>
-              <img src={ticket_icon} alt="Ticket Icon" className={style["ticket_box"]} />
-              <div style={{ color: "#FF6966" }}>별별 티켓</div>
+              <div className={style["ticket_wrapper"]}>
+                <img src={ticket_icon} alt="Ticket Icon" className={style["ticket_box"]} />
+                <div style={{ color: "#FF6966" }}>별별 티켓</div>
+              </div>
               <div>{myData.credit} 개</div>
             </div>
 
@@ -99,7 +129,10 @@ function MyPage() {
                 <div className={style["text_box"]}>
                   <p>나의최애</p>
                   <div className={style["bias_name"]}>{mySoloBias.bname}</div>
-                  <div className={style["season_star_dust"]}>이번 시즌에 기여한 별가루</div>
+                  <div className={style["powder_wrapper"]}>
+                    <img src={star_powder} alt="star_powder" className={style["poweder_box"]} />
+                    <div className={style["season_star_dust"]}>이번 시즌에 기여한 별가루</div>
+                  </div>
                   <h2>{myData.solo_point} pt</h2>
                   <button className={style["funding_btn"]}>펀딩</button>
                 </div>
@@ -122,7 +155,7 @@ function MyPage() {
               </div>
               <div className={style["half_box"]}>
                 <div className={style["text_box"]}>
-                  {myGroupBias.bname === "" ? <div className={style["bias_name"]}>아직 최애가 없어요</div> : <div className={style["bias_name"]}>{myGroupBias.bname}</div>}
+                  {myGroupBias.bname === "" ? <div className={style["not_name"]}>아직 최애가 없어요</div> : <div className={style["bias_name"]}>{myGroupBias.bname}</div>}
                   <div className={style["support_text"]}>최애를 정하고 지지해보는건 어떨까요?</div>
 
                   {/* <div className={style['bias_name']}>{myGroupBias.bname === '' && '아직 최애없음'}</div> */}
@@ -136,13 +169,37 @@ function MyPage() {
 
           <div className={`${style["base_box"]} ${style["height_auto"]}`}>
             <h4>서비스</h4>
-            <div className={style["grid_container"]}>
-              <div onClick={(e) => handleMovePage(e, "/galaxy")}>은하리그</div>
-              <div>노바 펀딩</div>
-              <div>노바 재판</div>
-              <div>노바 알고리즘</div>
+            <div className={style["grid_container_right"]}>
+              <div className={style["item"]} onClick={(e) => handleMovePage(e, "/galaxy")}>
+                <div className={style["icon_text_wrapper"]}>
+                  <img src={galaxyleague} alt="galaxyleague" className={style["icon_box"]} />
+                  <span>은하 리그</span>
+                </div>
+              </div>
+
+              <div className={style["item"]}>
+                <div className={style["icon_text_wrapper"]}>
+                  <img src={funding_icon} alt="funding_Icon" className={style["icon_box"]} />
+                  <span>노바 펀딩</span>
+                </div>
+              </div>
+
+              <div className={style["item"]}>
+                <div className={style["icon_text_wrapper"]}>
+                  <img src={judge_icon} alt="judge_icon" className={style["icon_box"]} />
+                  <span>노바 재판</span>
+                </div>
+              </div>
+
+              <div className={style["item"]}>
+                <div className={style["icon_text_wrapper"]}>
+                  <img src={novaAL_icon} alt="novaAL_icon" className={style["icon_box"]} />
+                  <span>노바 알고리즘</span>
+                </div>
+              </div>
             </div>
           </div>
+
           <div className={`${style["base_box"]} ${style["height_auto"]}`}>
             <h4>나의 활동</h4>
             <div className={style["my_activity"]}>
@@ -194,6 +251,13 @@ function MyPage() {
               </div>
             </div>
           </div>
+          <div
+            className={`${style["logout_box"]}`}
+            onClick={handleLogout}
+            style={{ cursor: "pointer" }} // 클릭 가능한 요소처럼 보이게 하기
+          >
+            로그아웃
+          </div>
         </>
       )}
 
@@ -210,10 +274,10 @@ function MyPage() {
                 <div className={style["info_items"]}>{myData.email}</div>
 
                 <div className={style["info_items"]}>나이</div>
-                <div className={style["info_items"]}>{myData.age}</div>
+                <div className={style["info_items"]}>{myData.age}세</div>
 
                 <div className={style["info_items"]}>성별</div>
-                <div className={style["info_items"]}>{myData.gender}</div>
+                <div className={style["info_items"]}>{myData.gender === "f" ? "여성" : myData.gender === "m" ? "남성" : ""}</div>
 
                 <div className={style["info_items"]}>별별 티켓 보유량</div>
                 <div className={style["info_items"]}>{myData.credit}개</div>
