874410dd4d330117a6b47522b33800ab60e902f5
betteree <kkne1017@gmail.com>
2024-10-23 10:51:04 +0900

    미리보기창 수정

--- nova_web/src/pages/MainPage/MainPart.js
+++ nova_web/src/pages/MainPage/MainPart.js
@@ -1,6 +1,13 @@
+import React, { useState } from "react";
 import style from "./MainPart.module.css";
 
 export default function MainPart() {
+  const [isActive, setIsActive] = useState(false); // 버튼의 상태를 관리
+
+  const handleClick = () => {
+    setIsActive((prev) => !prev); // 상태 토글
+  };
+
   return (
     <div className={style["wrap-container"]}>
       <div className={style["top-area"]}>
@@ -8,7 +15,18 @@ export default function MainPart() {
           <header>[ 시연 ] 관련 인기 해시태그</header>
           <div>화살표</div>
         </div>
-        <div className={style["tag-container"]}>버튼들</div>
+        <div className={style["tag-container"]}>
+          <button
+            className={style["hashtag-text"]}
+            onClick={handleClick}
+            style={{
+              backgroundColor: isActive ? "#051243" : "#373737",
+              cursor: "pointer",
+            }}
+          >
+            #시연시연시연시연시연
+          </button>
+        </div>
       </div>
 
       <div className={style["main-area"]}>
