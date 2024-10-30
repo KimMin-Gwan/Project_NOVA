e83399641866080c14202b925d8d6677e1d0ea20
betteree <kkne1017@gmail.com>
2024-10-30 15:10:20 +0900

    전체 글 레이아웃

--- nova_web/src/pages/MainPage/AllPost.js
+++ nova_web/src/pages/MainPage/AllPost.js
@@ -6,10 +6,33 @@ export default function AllPost() {
     <div className={style["wrap-container"]}>
       <div className={style["top-area"]}>
         <div className={style["content-title"]}>
-          <header className={style["header-text"]}>최근 인기 게시글</header>
+          <header className={style["header-text"]}>전체 글</header>
           <img src={more_icon} className={style["more-icon"]}></img>
-
-          <div className={style["main-area"]}></div>
+        </div>
+        <div className={style["main-area"]}>
+          <ul className={style["all-list"]}>
+            <li>
+              <div className={style["all-img"]}>이미지</div>
+              <div className={style["all-text"]}>
+                <span># 해시태그</span>
+                <p>충주맨이 말하는 공무원 과장님 모시는 방법</p>
+              </div>
+            </li>
+            <li>
+              <div className={style["all-img"]}>이미지</div>
+              <div className={style["all-text"]}>
+                <span># 해시태그</span>
+                <p>충주맨이 말하는 공무원 과장님 모시는 방법</p>
+              </div>
+            </li>
+            <li>
+              <div className={style["all-img"]}>이미지</div>
+              <div className={style["all-text"]}>
+                <span># 해시태그</span>
+                <p>충주맨이 말하는 공무원 과장님 모시는 방법</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
