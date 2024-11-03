00ac3dd4bdffccaaf28831fd7aedda067749f747
김민관 <alsrhks2508@naver.com>
2024-11-03 18:03:43 +0900

    Update core_system_view.py

--- server/src/view/core_system_view.py
+++ server/src/view/core_system_view.py
@@ -334,13 +334,13 @@ class Core_Service_View(Master_View):
             request_manager = RequestManager()
 
             form_data = await request.form()
+            image_files: List[UploadFile] = form_data.getlist("images")
     
-            # 이미지 확인을 위한 디버그 출력
-            image_files = form_data.getlist("images") 
-
-            print(image_files)
+            image_names = [image.filename for image in images]
+            print("Image filenames:", image_names)
 
             return "hello"
+
             if images is None or len(images) == 0:
                 image_names = []
                 imgs = []
