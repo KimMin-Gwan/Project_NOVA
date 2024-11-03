7c370c59f2846f9972617df360c8d257d61bc0b5
김민관 <alsrhks2508@naver.com>
2024-11-04 00:12:01 +0900

    Update temp.py

--- server/test/temp.py
+++ server/test/temp.py
@@ -1,36 +1,5 @@
-import pandas as pd
+
+data = ["1", "2", "3", "4", "5"]
 
 
-class SearchEngine:
-    def __init__(self):
-        self.search_module = SearchModule()
-
-
-    def try_search_feed(self, keyword):
-        self.search_module.get_new_feed(keyword=keyword, type="all")
-
-
-
-class SearchModule:
-    def __init__(self):
-        self.feed_data_frame = FeedDataframe()
-
-
-    def get_new_feed(self, keyword, type):
-        if keyword == "":
-            return None
-        else:
-            self.feed_data_frame.search_feed(keyword=keyword, type=type)
-
-
-
-class FeedDataframe:
-    def __init__(self):
-        self.df = pd.DataFrame()
-
-    def search_feed(self, keyword, type):
-        result = None
-        try:
-            result = df.get[keyword]
-        finally:
-            return result
+print(data[:2])
