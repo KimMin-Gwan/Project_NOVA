296f8e3bfe66c1c4c9bf8e2c66623b9dac533c3b
sunjuwhan <mathsun0011@gmail.com>
2024-11-09 21:39:10 +0900

    update feed

--- server/src/others/search_engine.py
+++ server/src/others/search_engine.py
@@ -11,8 +11,9 @@ import  datetime
 # 아래는 검색 엔진
 class SearchEngine:
     def __init__(self, database:Local_Database):
-        self.__search_manager = SearchManager(database=database)
-        self.__recommand_manager = RecommandManager(database=database)
+        self.__feed_algorithm= FeedAlgorithm(database=database)
+        self.__search_manager = SearchManager(database=database,self.__feed_algorithm)
+        self.__recommand_manager = RecommandManager(database=database,self.__feed_algorithm)
         self.db=database
     def try_serach_feed(self, target_type="hashtag", target = "", num_feed=10 ):
 
@@ -49,46 +50,6 @@ class SearchEngine:
             result = self.__recommand_manager.get_recommand_hashtags(user, bias, num_hashtag)
 
         return result 
-
-class SearchManager:
-    def __init__(self, database):
-        self.__database = database
-
-    def search_feed_with_hashtag(self, hashtag, num_feed=10) -> list:
-        
-        #hash tag로 찾는건데
-        #1. 해쉬태그로 좋아요많은순으로
-        #2. 해쉬태그로 최신순으로
-        #3. 
-        return 
-
-    def search_feed_with_fid(self, fid, num_feed=10) -> list:
-        return 
-
-    def search_feed_with_user(self, user, num_feed=10) -> list:
-        return 
-
-    def search_feed_with_string(self, string, num_feed=10) -> list:
-        return 
-
-class RecommandManager:
-    def __init__(self, database):
-        self.__database = database
-        self.__feed_algorithm = FeedAlgorithm(database=database)
-    def get_best_hashtags(self, num_hashtag=10) -> list:
-        return 
-
-    def get_next_feeds(self, feed, user, num_feed=1) -> list:
-        return 
-
-    def get_recommand_hashtags(self, user, bias, num_hashtag=4) -> list:
-        return 
-
-    def get_best_feeds(self, num_feed=10) -> list: 
-        return 
-
-
-
 """
 case 1) 첫 page에서 HASH TAG로 좋아요 순으로 보여줘야함
     -get_all_feed() 모든 피드를 가져오고
@@ -107,13 +68,34 @@ class FeedAlgorithm:
         self.__date_format = "%Y/%m/%d-%H:%M:%S"
     def get_all_feed(self):
         self.__now_all_feed=self.__database.get_all_data(target="fid")
-    #start_index는 이전에 내가 return해준index + num_of_feed , 그래야 이어서 돌려줌 
-    def get_feed_hashtag_with_times(self,start_index, hashtag:str,num_of_feed=10) ->int:   #index를 return해주고 받아야함
-        pass    
+    
+    
+
+    
+    #start_index는 이전에 내가 return해준index - num_of_feed , 그래야 이어서 돌려줌 
+    def get_feed_hashtag_with_times(self,start_index, hashtag:str,num_of_feed=10) :   #맨처음 화면에서 HashTag누르면 실행되는곳, 최신순으로 알맞는 데이터 return 해준다.
+        feed_data=[]
+        #그냥 HashTag , FEED ID -> Table 있으면 SELECT * FROM FEED ID where HASHTAG = hashtag 
+        #이 데이터로 FEED INDEX 가져오면 참좋은데 
+        
+        self.get_all_feed()  #실행되면 update   
+        for item in self.__now_all_feed:
+            if hashtag not in item["hashtag"] :
+                continue
+            else:
+                feed_data.append(item)
+                
+        if start_index<=num_of_feed:
+            return (feed_data[start_index:0:-1] ,0)  #FEED_DATA (list)  , INDEX(0)  
+        else:
+            return (feed_data[start_index:start_index-num_of_feed:-1],start_index-num_of_feed)
+    
+    
+    
+    
     
     def get_feed_hashtag_with_likes(self,hashtag:str,num_of_feed=10):  #가장 첫 page에서 hash tag눌렀을때 보여줘야할곳 
         feed_data=[]
-        result=[]
         self.get_all_feed()
         now_batch_index=0
         now_time=datetime.now()
@@ -123,7 +105,10 @@ class FeedAlgorithm:
                 #혹시라도 While문 계속 돌수도 있으니까
                 feed_data=sorted(feed_data,key = lambda x : x[1], reverse=True)
                 break
+            
             for item in self.__now_all_feed:
+                if hashtag not in item["hashtag"]:  #해쉬태그 없으면 pass
+                    continue
                 #날짜 형식  안맞는거같아서 바꾸공
                 post_date =datetime.datetime.strptime(item["date"],self.__date_fromat)
                 #차이나는 만큼 
@@ -139,10 +124,101 @@ class FeedAlgorithm:
                 else:
                     now_batch_index+=1
         return feed_data
-                
-    def get_like_feed(self):
+    
+    
+    def get_feed_with_fid(self,fid,num_of_feed=1) :  #더보기 기능?
+        self.get_all_feed()
+        feed_data=[]
+        for item in self.__now_all_feed:
+            if fid == item["fid"]:
+                feed_data.append(item)
+                break
+        return feed_data
+    
+    
+    
+    
+    def get_feed_with_user(self,uid,num_of_feed=10):  #유저가 작성한 최신순으로
+        self.get_all_feed()
+        feed_data=[]
+        feed_num=0
+        for item in reversed(self.__now_all_feed):
+            if uid==item["uid"]:
+                feed_data.append(item)
+                feed_num+=1
+            if feed_num==num_of_feed:
+                break
+        return feed뭔지모르겠
+    
+    
+    
+    def get_feed_with_string(self,string,num_of_feeds=10) :  #본문 내용 검색
+        self.get_all_feed()
+        feed_data=[]
+        feed_num=0
+        for item in reversed(self.__now_all_feed):
+            if string in item["body"]:
+                feed_data.append(item)
+                feed_num+=1
+            if feed_num==num_of_feeds:
+                break
+        return feed_data
+    
+    
+    
+    def get_like_feed(self):  #????뭔지모르겠
         pass
 
+
+
+
+
+class SearchManager:
+    def __init__(self, database,feed_algorithm : FeedAlgorithm):
+        self.__database = database
+        self.__feed_algorithm=feed_algorithm
+        
+        
+    #type likes, time
+    def search_feed_with_hashtag(self, hashtag,search_type="likes", num_feed=10) -> list:
+        
+        #hash tag로 찾는건데
+        #1. 해쉬태그로 좋아요많은순으로
+        #2. 해쉬태그로 최신순으로
+        #3. 
+        if search_type=="likes":
+            return self.__feed_algorithm.get_feed_hashtag_with_likes(hashtag,num_feed)  #list만 return
+        elif search_type=="time":
+            return self.__feed_algorithm.get_feed_hashtag_with_times(여기에 몇번 index까지 했는지,hashtag,num_feed)   #list, index
+
+    def search_feed_with_fid(self, fid, num_feed=10) -> list:   #feed id로 return해주는거 더보기 같음  아마num=1이 맞는거같은데
+        return  self.__feed_algorithm.get_feed_with_fid(fid,num_feed)
+
+    def search_feed_with_user(self, user, num_feed=10) -> list:   #User가 작성한 피드 돌려주는거? 
+        return  self.__feed_algorithm.get_feed_with_user(user,num_feed)
+
+    def search_feed_with_string(self, string, num_feed=10) -> list:   #본문 내용을 가지고 찾는거같음
+        return self.__feed_algorithm.get_feed_with_string(string,num_feed)
+    
+    
+    
+    
+    
+class RecommandManager:
+    def __init__(self, database,feed_algorithm):
+        self.__database = database
+        self.__feed_algorithm = feed_algorithm
+    def get_best_hashtags(self, num_hashtag=10) -> list:
+        return 
+
+    def get_next_feeds(self, feed, user, num_feed=1) -> list:
+        return 
+
+    def get_recommand_hashtags(self, user, bias, num_hashtag=4) -> list:
+        return 
+
+    def get_best_feeds(self, num_feed=10) -> list: 
+        return 
 
 
 
--- test/juwhan.py
+++ test/juwhan.py
@@ -19,23 +19,29 @@
 
 
 
-import heapq
+# import heapq
 
-# 우선순위 큐 초기화
-priority_queue = []
+# # 우선순위 큐 초기화
+# priority_queue = []
 
-# 데이터 샘플: 딕셔너리 리스트
-data = [
-    {"id": 1, "likes": 50, "name": "item1"},
-    {"id": 2, "likes": 30, "name": "item2"},
-    {"id": 3, "likes": 40, "name": "item3"},
-]
+# # 데이터 샘플: 딕셔너리 리스트
+# data = [
+#     {"id": 1, "likes": 50, "name": "item1"},
+#     {"id": 2, "likes": 30, "name": "item2"},
+#     {"id": 3, "likes": 40, "name": "item3"},
+# ]
 
-# 딕셔너리를 우선순위 큐에 추가 (likes 값을 기준으로 정렬)
-for item in data:
-    heapq.heappush(priority_queue, (item['likes'], item))
+# # 딕셔너리를 우선순위 큐에 추가 (likes 값을 기준으로 정렬)
+# for item in data:
+#     heapq.heappush(priority_queue, (item['likes'], item))
 
-# 우선순위 큐에서 데이터 출력
-while priority_queue:
-    _, item = heapq.heappop(priority_queue)
-    print(item)
+# # 우선순위 큐에서 데이터 출력
+# while priority_queue:
+#     _, item = heapq.heappop(priority_queue)
+#     print(item)
+
+def test():
+    list_a=[1,2,3,4]
+    return list_a[3:0:-1]
+
+print(test() )
