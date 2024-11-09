412921eee11e86709d7846b7a8a2e84f010fbeea
sunjuwhan <mathsun0011@gmail.com>
2024-11-09 15:59:54 +0900

    hashtag
    좋아요/시간

--- server/src/others/search_engine.py
+++ server/src/others/search_engine.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from model.local_database_model import *
+import  datetime 
 # 검색엔진이 해야하는일
 
 # 1. 키워드를 통한 피드 검색
@@ -9,11 +10,11 @@ import pandas as pd
 
 # 아래는 검색 엔진
 class SearchEngine:
-    def __init__(self, database):
+    def __init__(self, database:Local_Database):
         self.__search_manager = SearchManager(database=database)
         self.__recommand_manager = RecommandManager(database=database)
-
-    def try_serach_feed(self, target_type="hashtag", target = "", num_feed=10 ):\
+        self.db=database
+    def try_serach_feed(self, target_type="hashtag", target = "", num_feed=10 ):
 
         result = None
 
@@ -25,9 +26,10 @@ class SearchEngine:
             result = self.__serach_manager.search_feed_with_user(user=target)
         elif target_type == "string":
             result = self.__serach_manager.search_feed_with_string(string=target)
+            
         return result
 
-    def try_get_hashtags(self, target_type="default",, user=None, bias=None, num_hashtag):
+    def try_get_hashtags(self, num_hashtag,target_type="default", user=None, bias=None):
         result = []
 
         if target_type == "default":
@@ -37,21 +39,27 @@ class SearchEngine:
 
         return result
 
-    def try_recommand_feed(self, target_type="default", feed=None, user=None, bias=None, num_hashtag):
+    def try_recommand_feed(self, num_hashtag,target_type="default", feed=None, user=None, bias=None):
         result = []
 
-        if target_type == "short"
+        if target_type == "short":
+            
             result = self.__recommand_manager.get_next_feeds(feed, user, num_hashtag)
-        elif target_type == "best"::
+        elif target_type == "best":
             result = self.__recommand_manager.get_recommand_hashtags(user, bias, num_hashtag)
 
-        return reseult
+        return result 
 
 class SearchManager:
     def __init__(self, database):
         self.__database = database
 
     def search_feed_with_hashtag(self, hashtag, num_feed=10) -> list:
+        
+        #hash tag로 찾는건데
+        #1. 해쉬태그로 좋아요많은순으로
+        #2. 해쉬태그로 최신순으로
+        #3. 
         return 
 
     def search_feed_with_fid(self, fid, num_feed=10) -> list:
@@ -67,7 +75,6 @@ class RecommandManager:
     def __init__(self, database):
         self.__database = database
         self.__feed_algorithm = FeedAlgorithm(database=database)
-
     def get_best_hashtags(self, num_hashtag=10) -> list:
         return 
 
@@ -82,13 +89,60 @@ class RecommandManager:
 
 
 
+"""
+case 1) 첫 page에서 HASH TAG로 좋아요 순으로 보여줘야함
+    -get_all_feed() 모든 피드를 가져오고
+    -
+    -NUM_OF_FEED 갯수만큼 return 해야함
+    -이때 날짜는 1주일 1달 3달 6달 1년 2년 .....첫날짜 까지 계속해서 늘려감
+    -
+"""
+
 class FeedAlgorithm:
-    def __init__(self, database):
-        self.__database = database
+    def __init__(self, database:Local_Database):
+        self.__now_all_feed=[]
+        self.__database=database 
+        self.__find_feed=[]
+        self.__batch_size=[7,31,91,180,360,720]  #그이후는 시작일짜로 ...
+        self.__date_format = "%Y/%m/%d-%H:%M:%S"
+    def get_all_feed(self):
+        self.__now_all_feed=self.__database.get_all_data(target="fid")
+    #start_index는 이전에 내가 return해준index + num_of_feed , 그래야 이어서 돌려줌 
+    def get_feed_hashtag_with_times(self,start_index, hashtag:str,num_of_feed=10) ->int:   #index를 return해주고 받아야함
+        pass    
+    
+    def get_feed_hashtag_with_likes(self,hashtag:str,num_of_feed=10):  #가장 첫 page에서 hash tag눌렀을때 보여줘야할곳 
+        feed_data=[]
+        result=[]
+        self.get_all_feed()
+        now_batch_index=0
+        now_time=datetime.now()
+        while True:
+            if(now_batch_index==len(self.__batch_size)):
+                #끝까지 갔는데도 안나오면 그냥 return하자
+                #혹시라도 While문 계속 돌수도 있으니까
+                feed_data=sorted(feed_data,key = lambda x : x[1], reverse=True)
+                break
+            for item in self.__now_all_feed:
+                #날짜 형식  안맞는거같아서 바꾸공
+                post_date =datetime.datetime.strptime(item["date"],self.__date_fromat)
+                #차이나는 만큼 
+                delta_days=now_time-post_date
+
+                if delta_days<self.__batch_size[now_batch_index]:
+                    like_between_day=item["star"]/(delta_days+1)  #같은 시간이면 divided 0 임
+                    feed_data.append(like_between_day)
+                    
+                if len(feed_data)>=num_of_feed:
+                    feed_data=sorted(feed_data,key = lambda x : x[1], reverse=True)[:num_of_feed]
+                    break
+                else:
+                    now_batch_index+=1
+        return feed_data
+                
+    def get_like_feed(self):
         pass
 
-    
 
 
 
-# hash
---
+++ test/juwhan.py
@@ -0,0 +1,41 @@
+# from datetime import datetime
+
+# # 저장된 과거 날짜
+# past_date_str = "2024/06/02-21:08:30"
+
+# # 문자열을 datetime 객체로 변환
+# past_date = datetime.strptime(past_date_str, "%Y/%m/%d-%H:%M:%S")
+
+# # 오늘 날짜 가져오기
+# current_date = datetime.now()
+
+# # 날짜 차이 계산
+# date_difference = current_date - past_date
+
+# # 차이 출력
+# print(f"두 날짜 간의 차이는 {date_difference.days}일 {date_difference.seconds // 3600}시간 {(date_difference.seconds % 3600) // 60}분입니다.")
+
+
+
+
+
+import heapq
+
+# 우선순위 큐 초기화
+priority_queue = []
+
+# 데이터 샘플: 딕셔너리 리스트
+data = [
+    {"id": 1, "likes": 50, "name": "item1"},
+    {"id": 2, "likes": 30, "name": "item2"},
+    {"id": 3, "likes": 40, "name": "item3"},
+]
+
+# 딕셔너리를 우선순위 큐에 추가 (likes 값을 기준으로 정렬)
+for item in data:
+    heapq.heappush(priority_queue, (item['likes'], item))
+
+# 우선순위 큐에서 데이터 출력
+while priority_queue:
+    _, item = heapq.heappop(priority_queue)
+    print(item)
