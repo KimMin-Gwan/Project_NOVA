25879c876fb6307e3f7780db9e90294e304c062f
betteree <kkne1017@gmail.com>
2024-11-10 15:14:06 +0900

    레이아웃 수정

--- nova_web/src/pages/MainPage/AllPost.js
+++ nova_web/src/pages/MainPage/AllPost.js
@@ -10,11 +10,9 @@ export default function AllPost() {
       <div className={style["top-area"]}>
         <div className={style["content-title"]}>
           <header className={style["header-text"]}>전체 글</header>
-          <img src={more_icon} alt="more_icon" className={style["more-icon"]}
-            onClick={() => navigate("/feed_list?type=all")}
-          />
+          <img src={more_icon} alt="more_icon" className={style["more-icon"]} onClick={() => navigate("/feed_list?type=all")} />
         </div>
-        <div className={style["main-area"]}>
+        <div className={`${style["main-area"]} ${style["all-main-area"]}`}>
           <ul className={style["all-list"]}>
             <li>
               <div className={style["all-img"]}>이미지</div>
@@ -40,7 +38,24 @@ export default function AllPost() {
                 <footer className={style["like-comment"]}>좋아요 250개 | 댓글 20개</footer>
               </div>
             </li>
+            <li>
+              <div className={style["all-img"]}>이미지</div>
+              <div className={style["all-text"]}>
+                <span># 해시태그</span>
+                <p>충주맨이 말하는 공무원 과장님 모시는 방법</p>
+                <footer className={style["like-comment"]}>좋아요 250개 | 댓글 20개</footer>
+              </div>
+            </li>
+            <li>
+              <div className={style["all-img"]}>이미지</div>
+              <div className={style["all-text"]}>
+                <span># 해시태그</span>
+                <p>충주맨이 말하는 공무원 과장님 모시는 방법</p>
+                <footer className={style["like-comment"]}>좋아요 250개 | 댓글 20개</footer>
+              </div>
+            </li>
           </ul>
+          <button className={style["all_see-button"]}>전체보기</button>
         </div>
       </div>
     </div>
