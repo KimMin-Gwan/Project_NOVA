113375223fd2a738a77582bd4bff4ff6bae737e1
김민관 <alsrhks2508@naver.com>
2024-11-19 04:22:49 +0900

    Update search_engine.py

--- server/src/others/search_engine.py
+++ server/src/others/search_engine.py
@@ -727,6 +727,7 @@ class RecommandManager:
                 print(2)
                 for user_edge in user_node.edges:
                     print(3)
+                    print(user_edge.target_node)
                     feed_node:FeedNode = user_edge.target_node
                     print(feed_node)
                     for feed_edge in feed_node.edges:
