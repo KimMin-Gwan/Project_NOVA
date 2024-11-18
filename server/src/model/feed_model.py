df95715d5ba44d3abb62b4294fa1cd6d2e1f464c
김민관 <alsrhks2508@naver.com>
2024-11-19 01:44:27 +0900

    Update feed_model.py

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -302,6 +302,9 @@ class FeedSearchModel(FeedModel):
             self.__history.append(str(fid))
             second_fid = feed_search_engine.try_recommand_feed(fid=str(fid),
                                                   history=self.__history)
+            
+            print(str(fid))
+            print(second_fid)
 
             feed_datas = self._database.get_datas_with_ids(target_id="fid", ids=[str(fid), second_fid])
             for feed_data in feed_datas:
