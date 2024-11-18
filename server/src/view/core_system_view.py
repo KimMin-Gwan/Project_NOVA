3ce1f107798539b361745d89bb11d51d9b9c6091
김민관 <alsrhks2508@naver.com>
2024-11-19 01:57:37 +0900

    Update core_system_view.py

--- server/src/view/core_system_view.py
+++ server/src/view/core_system_view.py
@@ -330,7 +330,7 @@ class Core_Service_View(Master_View):
         @self.__app.post('/feed_explore/get_next_feed')
         def get_next_feed(request:Request, raw_request:dict):
             request_manager = RequestManager()
-            print(raw_request)
+            pprint(raw_request)
             data_payload = ShortFeedRecommendRequest(request=raw_request)
 
             request_manager.try_view_management(data_payload=data_payload, cookies=request.cookies)
