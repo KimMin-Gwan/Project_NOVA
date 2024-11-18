4064ea821996800533355b320049c15810a2109d
김민관 <alsrhks2508@naver.com>
2024-11-19 00:22:30 +0900

    Update nova_server.py

--- server/src/view/nova_server.py
+++ server/src/view/nova_server.py
@@ -79,8 +79,8 @@ class NOVAVerification:
         #exp_checker = Thread(target=self._check_expiration)
         #exp_checker.start()
 
-        #self.loop = asyncio.get_event_loop()
-        #self.loop.create_task(self._check_expiration())
+        self.loop = asyncio.get_event_loop()
+        self.loop.create_task(self._check_expiration())
     
     def get_temp_user(self):
         for data in self.__temp_user:
@@ -139,6 +139,7 @@ class NOVAVerification:
         
     # 만료시간 체크해서 제거
     async def _check_expiration(self):
+        print("im not expired")
         while True:
             time.sleep(1)
             for user in self.__temp_user:
