51bfaa98aedd699cb29b8d05c2a6b9de9333daa8
김민관 <alsrhks2508@naver.com>
2024-12-24 13:18:49 +0900

    vh -> dvh 로 수정

---
+++ test/sample.js
@@ -0,0 +1,436 @@
+import React from "react";
+import re1r12 from "./1re1r1-2.png";
+import re1r13 from "./1re1r1-3.png";
+import x15 from "./15.png";
+import x16 from "./16.png";
+import x17 from "./17.png";
+import x18 from "./18.png";
+import x19 from "./19.png";
+import x20 from "./20.png";
+import x21 from "./21.png";
+import x22 from "./22.png";
+import x23 from "./23.png";
+import x24 from "./24.png";
+import x25 from "./25.png";
+import x26 from "./26.png";
+import x27 from "./27.png";
+import x28 from "./28.png";
+import x29 from "./29.png";
+import x30 from "./30.png";
+import x31 from "./31.png";
+import x32 from "./32.png";
+import x33 from "./33.png";
+import x34 from "./34.png";
+import { Frame } from "./Frame";
+import battery from "./battery.png";
+import imageRemovebgPreview11 from "./image-removebg-preview-1-1.png";
+import imageRemovebgPreview12 from "./image-removebg-preview-1-2.png";
+import imageRemovebgPreview13 from "./image-removebg-preview-1-3.png";
+import image from "./image.svg";
+import leftSide from "./left-side.png";
+import maskGroup from "./mask-group.png";
+import mobileSignal from "./mobile-signal.svg";
+import rectangle4700 from "./rectangle-4700.png";
+import search from "./search.svg";
+import "./style.css";
+import vector2 from "./vector-2.svg";
+import vector3 from "./vector-3.svg";
+import vector from "./vector.svg";
+import wifi from "./wifi.svg";
+
+export const Screen = () => {
+  return (
+    <div className="screen">
+      <div className="div-2">
+        <div className="overlap">
+          <div className="rectangle" />
+
+          <div className="group">
+            <p className="p">
+              <span className="text-wrapper">오늘의 베스트 </span>
+
+              <span className="span">피드</span>
+            </p>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="frame-2">
+            <div className="checkbox">
+              <div className="text-wrapper-3">
+                보고 싶은 최애를 검색해보세요
+              </div>
+            </div>
+
+            <img className="search" alt="Search" src={search} />
+          </div>
+
+          <div className="rectangle-2" />
+
+          <div className="rectangle-2" />
+
+          <div className="rectangle-3" />
+
+          <div className="rectangle-4" />
+
+          <p className="text-wrapper-4">좋아요 50개 | 댓글 0개</p>
+
+          <img
+            className="image-removebg"
+            alt="Image removebg"
+            src={imageRemovebgPreview12}
+          />
+
+          <img className="element" alt="Element" src={re1r12} />
+
+          <p className="text-wrapper-5">
+            이번 특별 방송은 진짜 개웃김 ㅋㅋㅋ
+            <br />
+            포스터에도 재갈금자 모모구 ㅇㅈㄹ ㅋㅋㅋㅋ 개웃...
+          </p>
+
+          <div className="text-wrapper-6">
+            #미츄&nbsp;&nbsp; #이오몽&nbsp;&nbsp; #미녕이
+          </div>
+
+          <div className="rectangle-5" />
+
+          <div className="rectangle-5" />
+
+          <div className="rectangle-6" />
+
+          <div className="rectangle-7" />
+
+          <p className="text-wrapper-7">좋아요 50개 | 댓글 0개</p>
+
+          <img
+            className="img"
+            alt="Image removebg"
+            src={imageRemovebgPreview13}
+          />
+
+          <img className="element-rer" alt="Element" src={re1r13} />
+
+          <p className="text-wrapper-8">
+            이번 특별 방송은 진짜 개웃김 ㅋㅋㅋ
+            <br />
+            포스터에도 재갈금자 모모구 ㅇㅈㄹ ㅋㅋㅋㅋ 개웃...
+          </p>
+
+          <div className="text-wrapper-9">
+            #미츄&nbsp;&nbsp; #이오몽&nbsp;&nbsp; #미녕이
+          </div>
+
+          <img className="vector" alt="Vector" src={image} />
+
+          <div className="TOP-wrapper">
+            <p className="p">
+              <span className="text-wrapper">주간 </span>
+
+              <span className="span">TOP 100</span>
+            </p>
+          </div>
+
+          <div className="group-2">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-2">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-2">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-2">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <img className="vector-2" alt="Vector" src={vector2} />
+
+          <div className="group-3">
+            <div className="text-wrapper-10">전체글</div>
+          </div>
+
+          <div className="group-4">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-4">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-4">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-4">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <img className="vector-3" alt="Vector" src={vector3} />
+
+          <div className="rectangle-8" />
+
+          <img className="element-2" alt="Element" src={x15} />
+
+          <img className="element-2" alt="Element" src={x16} />
+
+          <div className="text-wrapper-11"># 충주맨</div>
+
+          <div className="text-wrapper-12">#하극상</div>
+
+          <p className="text-wrapper-13">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-9" />
+
+          <img className="element-3" alt="Element" src={x17} />
+
+          <img className="element-3" alt="Element" src={x18} />
+
+          <div className="text-wrapper-14"># 충주맨</div>
+
+          <div className="text-wrapper-15">#하극상</div>
+
+          <p className="text-wrapper-16">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-10" />
+
+          <img className="element-4" alt="Element" src={x19} />
+
+          <img className="element-4" alt="Element" src={x20} />
+
+          <div className="text-wrapper-17"># 충주맨</div>
+
+          <div className="text-wrapper-18">#하극상</div>
+
+          <p className="text-wrapper-19">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-11" />
+
+          <img className="element-5" alt="Element" src={x21} />
+
+          <img className="element-5" alt="Element" src={x22} />
+
+          <div className="text-wrapper-20"># 충주맨</div>
+
+          <div className="text-wrapper-21">#하극상</div>
+
+          <p className="text-wrapper-22">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-10" />
+
+          <img className="element-4" alt="Element" src={x23} />
+
+          <img className="element-4" alt="Element" src={x24} />
+
+          <div className="text-wrapper-17"># 충주맨</div>
+
+          <div className="text-wrapper-18">#하극상</div>
+
+          <p className="text-wrapper-19">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-11" />
+
+          <img className="element-5" alt="Element" src={x25} />
+
+          <img className="element-5" alt="Element" src={x26} />
+
+          <div className="text-wrapper-20"># 충주맨</div>
+
+          <div className="text-wrapper-21">#하극상</div>
+
+          <p className="text-wrapper-22">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <Frame className="frame-1704" frameClassName="frame-instance" />
+          <div className="rectangle-12" />
+
+          <div className="rectangle-12" />
+
+          <img className="element-6" alt="Element" src={x27} />
+
+          <img className="element-6" alt="Element" src={x28} />
+
+          <img className="element-6" alt="Element" src={x29} />
+
+          <img className="element-6" alt="Element" src={x30} />
+
+          <div className="text-wrapper-23"># 충주맨</div>
+
+          <div className="text-wrapper-23"># 충주맨</div>
+
+          <div className="text-wrapper-24">#하극상</div>
+
+          <div className="text-wrapper-24">#하극상</div>
+
+          <p className="text-wrapper-25">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <p className="text-wrapper-25">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-13" />
+
+          <div className="rectangle-13" />
+
+          <img className="element-7" alt="Element" src={x31} />
+
+          <img className="element-7" alt="Element" src={x32} />
+
+          <img className="element-7" alt="Element" src={x33} />
+
+          <img className="element-7" alt="Element" src={x34} />
+
+          <div className="text-wrapper-26"># 충주맨</div>
+
+          <div className="text-wrapper-26"># 충주맨</div>
+
+          <div className="text-wrapper-27">#하극상</div>
+
+          <div className="text-wrapper-27">#하극상</div>
+
+          <p className="text-wrapper-28">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <p className="text-wrapper-28">
+            충주맨이 말하는 공무원 과장님 모시는...
+          </p>
+
+          <div className="rectangle-14" />
+
+          <div className="text-wrapper-29">실시간 랭킹</div>
+        </div>
+
+        <div className="status-bar-iphone-x">
+          <div className="right-side">
+            <img className="battery" alt="Battery" src={battery} />
+
+            <img className="wifi" alt="Wifi" src={wifi} />
+
+            <img
+              className="mobile-signal"
+              alt="Mobile signal"
+              src={mobileSignal}
+            />
+          </div>
+
+          <img className="left-side" alt="Left side" src={leftSide} />
+        </div>
+
+        <div className="frame-3">
+          <p className="text-wrapper-30">최애가 가장 빛날 수 있는 공간</p>
+        </div>
+
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <div className="text-wrapper-31">NOVA</div>
+
+            <div className="ellipse" />
+
+            <div className="ellipse-2" />
+          </div>
+        </div>
+
+        <img className="mask-group" alt="Mask group" src={maskGroup} />
+
+        <img className="rectangle-15" alt="Rectangle" src={rectangle4700} />
+
+        <div className="rectangle-16" />
+
+        <div className="rectangle-17" />
+
+        <div className="rectangle-18" />
+
+        <div className="group-5">
+          <p className="p">
+            <span className="text-wrapper">내가 가장 보고싶은 </span>
+
+            <span className="span">최애</span>
+          </p>
+        </div>
+
+        <div className="overlap-2">
+          <div className="group-6">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-6">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-6">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+
+          <div className="group-6">
+            <div className="text-wrapper-2">📌</div>
+          </div>
+        </div>
+
+        <div className="text-wrapper-32">시연</div>
+
+        <div className="overlap-3">
+          <div className="text-wrapper-33">밍바라기</div>
+
+          <p className="text-wrapper-34">
+            시연이 너무 이쁜데?!?!?!?!?!
+            <br />
+            시연이 나이 몇살이에요 ???
+          </p>
+
+          <div className="text-wrapper-35">(더보기)</div>
+
+          <p className="text-wrapper-36">좋아요 50개 | 댓글 0개</p>
+
+          <div className="text-wrapper-37">
+            #시요밍&nbsp;&nbsp;#큐떱&nbsp;&nbsp;#이시연&nbsp;&nbsp;#시연
+          </div>
+        </div>
+
+        <div className="rectangle-19" />
+
+        <div className="rectangle-20" />
+
+        <img
+          className="image-removebg-2"
+          alt="Image removebg"
+          src={imageRemovebgPreview11}
+        />
+
+        <img className="vector-4" alt="Vector" src={vector} />
+      </div>
+    </div>
+  );
+};
+
+
