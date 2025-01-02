99c1f3b941a14e0e27b2e50ff0d67ba8014daf26
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-01-02 14:35:50 +0900

    Data_domain 댓글 클래스. 새로운 멤버가 추가 (target_cid), 그에 따른 딕셔너리 생성 함수, 딕셔너리에서 읽어서 만드는 함수를 수정함.

--- server/src/others/data_domain.py
+++ server/src/others/data_domain.py
@@ -357,7 +357,7 @@ class Banner(SampleDomain):
 
 class Comment(SampleDomain):
     def __init__(self, cid="", fid="", uid="", uname="",
-                 body="", date="", like=0, state="y", like_user=[], mention=""):
+                 body="", date="", like=0, state="y", like_user=[], mention="", target_cid=""):
         self.cid = cid
         self.fid = fid
         self.uid = uid
@@ -369,6 +369,7 @@ class Comment(SampleDomain):
         self.like_user = copy.copy(like_user)
         self.num_like_user = len(self.like_user)
 
+        self.target_cid = target_cid            # 대댓글을 달 위치 cid
         self.owner = False
         self.mention = mention
 
@@ -383,6 +384,9 @@ class Comment(SampleDomain):
             self.like = dict_data['like']
             self.state = dict_data['state']
             self.like_user= copy.copy(dict_data['like_user'])
+            self.target_cid = dict_data['target_cid']
+            self.owner = dict_data['owner']
+            self.mention = dict_data['mention']
         except KeyError as e:
             raise DictMakingError(error_type=f"Missing key: {str(e)}")
 
@@ -397,7 +401,9 @@ class Comment(SampleDomain):
             "like": self.like,
             "state": self.state,
             "like_user": copy.copy(self.like_user),
-            "owner" : self.owner
+            "target_cid": self.target_cid,
+            "owner" : self.owner,
+            "mention": self.mention,
         }
 
 # 유저 특화 시스템 구성을 위한 관리 유저
