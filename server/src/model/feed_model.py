0480d2e88387f42f37609613d590f25e30b61d25
김민관 <alsrhks2508@naver.com>
2025-01-10 12:48:11 +0900

    Update feed_model.py

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -496,14 +496,14 @@ class CommunityFeedModel(FeedModel):
         if bid =="":
             fid_list, self.__last_fid = feed_search_engine.try_feed_with_bid_n_filtering(
                 target_bids=self._user.bids, board_type=board_type,
-                page_size=1, last_fid=last_fid, search_type="bias_and_board",
+                page_size=5, last_fid=last_fid, search_type="bias_and_board",
             )
         
         # bias를 선택했을 때
         else:
             fid_list, self.__last_fid = feed_search_engine.try_feed_with_bid_n_filtering(
                 target_bids=[bid], board_type=board_type,
-                page_size=1, last_fid=last_fid, search_type="bias_and_board",
+                page_size=5, last_fid=last_fid, search_type="bias_and_board",
             )
         
         # 보낼 데이터 만들어 주기
