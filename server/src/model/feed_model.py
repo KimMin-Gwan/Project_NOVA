5f51dc5904b0b171096fbcaf9a12458ee2a1b779
김민관 <alsrhks2508@naver.com>
2025-01-13 14:21:40 +0900

    출력 제거

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -499,8 +499,6 @@ class CommunityFeedModel(FeedModel):
                                              feed_manager:FeedManager,
                                              ):
         
-        print("board_type : ", board_type)
-        
         # bias를 선택하지 않았을 때
         if bid =="":
             fid_list, self.__last_fid = feed_search_engine.try_feed_with_bid_n_filtering(
