a05acf8886fac600972440fbb54b2dc828c9619d
김민관 <alsrhks2508@naver.com>
2025-01-13 12:09:37 +0900

    Update feed_manager.py

--- server/src/others/feed_manager.py
+++ server/src/others/feed_manager.py
@@ -1504,7 +1504,7 @@ class FeedManager:
     # Feed에 좋아요를 눌렀을 때의 작용
     def try_staring_feed(self, user:User, fid:str):
         feed = self.__try_staring_feed(user=user, fid=fid)
-        return feed
+        return [feed]
 
     # feed 와 상호작용 -> 관심 표시
     def __try_staring_feed(self, user:User, fid):
