6fdb54bfd9c358ab71cf1087e07b2486fb1eb720
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-01-23 14:39:44 +0900

    필터링 테스트 3

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -531,11 +531,13 @@ class FilteredFeedModel(FeedModel):
         #   왜 FClass 부터 먼저 진행하나요? -> 간단한 것부터 먼저 분류합니다.
         #
         fid_list = feed_search_engine.try_filtered_feed_with_option(fid_list=fid_list, option="fclass", keys=[fclass])
+        pprint(fid_list[:5])
 
         # 2차 필터링 : Category 별 분류를 진행합니다.
         # AD의 경우, 생각중
         fid_list = feed_search_engine.try_filtered_feed_with_option(fid_list=fid_list, option="category", keys=category)
 
+        pprint(fid_list[:5])
         # 마지막, 분류가 끝이 났으면 페이징을 진행합니다.
         fid_list, self._key = feed_manager.paging_fid_list(fid_list=fid_list, last_index=last_index, page_size=num_feed)
 
