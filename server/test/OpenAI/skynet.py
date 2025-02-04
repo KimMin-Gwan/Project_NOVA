9937fd3a0bbde0cc99305012406a8500b54c41f4
Vespoi <103725786+Vespoi@users.noreply.github.com>
2025-02-04 22:04:00 +0900

    GPT테스트

--- server/test/OpenAI/skynet.py
+++ server/test/OpenAI/skynet.py
@@ -1,6 +1,13 @@
 from openai import OpenAI
 #API 키 
-client = OpenAI(api_key="#######")
+client = OpenAI(api_key="###############")
+
+## 현재 자료의 문장이 완성형에 가까워 높은 문장 완성 성능을 보여주는 중
+## 커뮤니티에 게시된 자연어들이 제대로 처리 되는지 확인 하려면 완전히 박살난 문장 형식의 글이나 문맥 파악이 불가능한 자료가 필요
+## 초성, meme, 축약어 등 사전적 의미가 존재하지 않는 단어 처리 확인 필요
+## 욕설로 도배된 자료 처리 확인 필요
+## 많은(다양한) 데이터가 필요할 것으로 예상됨
+
 
 #gpt 사용하기 (본문)
 def rework(context):
@@ -26,7 +33,8 @@ def rework(context):
         {"role": "user", "content": "응답은 '원본'과 '변환문', 강도로 합니다."},        
         {"role": "user", "content": "응답의 원본과 변환문 또한 title과 content로 구성되어야 합니다."},
         {"role": "user", "content": "title과 content 모두 변환합니다."},
-        {"role": "user", "content": "강도는 비속어가 포함된 경우 2, 비속어는 포함되지 않으나 반말로 작성된 경우 1, 비속어 나 반말이 사용되지 않고 올바른 문장으로 작성 된 경우 0을 반환합니다"},
+        {"role": "user", "content": "강도는 비속어 또는 욕설이 포함된 경우 2, 비속어는 포함되나 않으나 욕설이 없이 작성된 경우 1, 비속어 또는 욕설이 사용되지 않고 작성 된 경우 0을 반환합니다"},
+        {"role": "user", "content": '고유 명사는 변환하지 않습니다.'},
         {"role": "user", "content": "변환된 문장엔 공격적인 단어가 적게 포함되어야 합니다."},
 
         #답변 제공
@@ -39,7 +47,7 @@ def rework(context):
         
     print(result)
 
-#gpt 사용하기 (트렌드)
+#gpt 사용하기 (트렌드) /공사중
 def trand(context):
     response = client.chat.completions.create(
         #gpt 모댈
@@ -70,7 +78,7 @@ def trand(context):
     result = response.choices[0].message.content
     print(result)
 
-#gpt 사용하기 (댓글)
+#gpt 사용하기 (댓글) /공사중
 def comment(context):
     response = client.chat.completions.create(
         #gpt 모댈
@@ -90,7 +98,7 @@ def comment(context):
         ]
     )
 
-#gpt 사용하기 (질문)
+#gpt 사용하기 (질문) /공사중
 def autoqna(context):
     response = client.chat.completions.create(
         #gpt 모댈
@@ -114,7 +122,7 @@ def autoqna(context):
 #변환(검열)할 텍스트
 context = {
     'title' : '',
-    'content' : 'ㅎㅇ'
+    'content' : '칸예가 입었으면 더 주목받을텐데 직접 입어라  쫄보새꺄'
 }
 
 
