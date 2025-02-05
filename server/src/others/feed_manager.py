877b7523db1b57a721f40b3f5f18487afe53a5ae
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-02-06 03:09:27 +0900

    reply 테스트 중 5

--- server/src/others/feed_manager.py
+++ server/src/others/feed_manager.py
@@ -1786,7 +1786,7 @@ class FeedManager:
             for comment in no_targeted_comments:
                 if comment.cid == targeted_comment.target_cid:
                     # 이미 추가가 되어있다면 넘어간다
-                    if self.__find_comment_in_comment_list(comments.reply, targeted_comment.cid) is not None:
+                    if self.__find_comment_in_comment_list(comment.reply, targeted_comment.cid) is not None:
                         continue
                     comment.reply.append(targeted_comment.get_dict_form_data())
 
