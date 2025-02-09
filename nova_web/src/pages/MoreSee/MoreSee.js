00f42596225e18bb7083a047f4b1dbcc54c9115c
betteree <kkne1017@gmail.com>
2025-02-09 21:07:20 +0900

    더보기 페이지

--- nova_web/src/pages/MoreSee/MoreSee.js
+++ nova_web/src/pages/MoreSee/MoreSee.js
@@ -5,24 +5,25 @@ import discord_icon from "./../../img/discord_color.png";
 import insta_icon from "./../../img/insta_color.png";
 import youtube_icon from "./../../img/youtube_color.png";
 import set_icon from "./../../img/setting_icon.png";
-import fav_icon from "./../../img/favset_icon.png";
-import noti_icon from "./../../img/noti_icon.png";
+import fav_icon from "./../../img/favset_icon.svg";
+import bug_icon from "./../../img/bug_icon.svg";
 import login_icon from "./../../img/login_icon.png";
 import mypage_icon from "./../../img/mypage_icon.png";
-import terms_icon from "./../../img/terms_icon.png";
+import terms_icon from "./../../img/terms_icon.svg";
 import business_logo from "./../../img/business_logo.png";
 import more_icon from "./../../img/Icon.png";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import popular_icon from "./../../img/popular.png";
-import month_funding from "./../../img/month-funding.png";
-import fav_like from "./../../img/fav-like.png";
-import duck_funding from "./../../img/duck-funding.png";
-import new_feed from "./../../img/new-feed.png";
-import guide_icon from "./../../img/guide.png";
-import short_feed from "./../../img/short-feed.png";
-import up_hashtag from "./../../img/up-hashtag.png";
+import new_moment from "./../../img/new_moment.svg";
+import today_up from "./../../img/today_up.svg";
+import week_up from "./../../img/week_up.svg";
+import all_post from "./../../img/all_post.svg";
+import new_post from "./../../img/new_post.svg";
+import fav_follow from "./../../img/fav_follow.svg";
+import fav_sub from "./../../img/fav_sub.svg";
+import notice from "./../../img/notice_more.svg";
 import { getModeClass } from "./../../App.js";
+
 function MoreSee({ onModeChange }) {
   const requestURL = {
     x: "https://x.com/sebacheong",
@@ -118,11 +119,7 @@ function MoreSee({ onModeChange }) {
               }
             }}
           >
-            <img
-              src={isLogin ? mypage_icon : login_icon}
-              alt="Arrow"
-              className={style.vector_login}
-            />
+            <img src={isLogin ? mypage_icon : login_icon} alt="Arrow" className={style.vector_login} />
             <p className={style.bodyText_login}>{isLogin ? "마이페이지" : "로그인"}</p>
           </div>
 
@@ -133,87 +130,65 @@ function MoreSee({ onModeChange }) {
               navigate("/");
             }}
           >
-            노바 플랫폼
+            게시판 목록
           </div>
           <hr></hr>
           <ul className={style.listContainer}>
             <li className={style.mainComponent} onClick={() => handlePage("/feed_list")}>
               <img src={fav_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>전체 피드</p>
+              <p className={style.bodyText}>최애 주제 게시판</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
             <li className={style.mainComponent} onClick={() => handlePage("/feed_page")}>
-              <img src={short_feed} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>숏피드</p>
+              <img src={today_up} alt="Arrow" className={style.vector} />
+              <p className={style.bodyText}>오늘의 급상승 게시글</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
             <li className={style.mainComponent} onClick={() => handlePage("/feed_list?type=best")}>
-              <img src={popular_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>오늘의 베스트 피드</p>
+              <img src={week_up} alt="Arrow" className={style.vector} />
+              <p className={style.bodyText}>주간 급상승 게시글</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
-            <li
-              className={style.mainComponent}
-              onClick={() => handlePage("/feed_list?type=weekly_best")}
-            >
-              <img src={up_hashtag} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>주간 TOP 100</p>
-              <img src={more_icon} alt="Arrow" className={style.more_vector} />
-            </li>
-            <li className={style.mainComponent} onClick={() => handlePage("/write_feed")}>
-              <img src={new_feed} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>새로운 피드 작성</p>
+            <li className={style.mainComponent} onClick={() => handlePage("/feed_list?type=weekly_best")}>
+              <img src={all_post} alt="Arrow" className={style.vector} />
+              <p className={style.bodyText}>전체 게시글</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
           </ul>
           <br></br>
-          <div
-            className={style["list-bar"]}
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/nova_funding");
-            }}
-          >
-            노바 펀딩
+          <div className={style["service-container"]}>
+            <h3>서비스</h3>
+            <section className={style["button-container"]}>
+              <button>
+                <img src={new_moment} alt="새로운 모멘트 작성" />
+                새로운 모멘트 작성
+              </button>
+              <button>
+                <img src={new_post} alt="새로운 포스트 작성" />
+                새로운 포스트 작성
+              </button>
+              <button>
+                <img src={fav_follow} alt="최애 주제 팔로우" />
+                최애 주제 팔로우
+              </button>
+              <button>
+                <img src={fav_sub} alt="최애 주제 신청" />
+                최애 주제 신청
+              </button>
+            </section>
           </div>
-          <hr></hr>
-          <ul className={style.listContainer}>
-            <li className={style.mainComponent}>
-              <img src={fav_like} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>최애 펀딩</p>
-              <img src={more_icon} alt="Arrow" className={style.more_vector} />
-            </li>
-            <li className={style.mainComponent}>
-              <img src={duck_funding} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>덕질 펀딩</p>
-              <img src={more_icon} alt="Arrow" className={style.more_vector} />
-            </li>
-            <li className={style.mainComponent}>
-              <img src={month_funding} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>이달의 펀딩 목록</p>
-              <img src={more_icon} alt="Arrow" className={style.more_vector} />
-            </li>
-            <li className={style.mainComponent}>
-              <img src={guide_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>펀딩 가이드</p>
-              <img src={more_icon} alt="Arrow" className={style.more_vector} />
-            </li>
-          </ul>
           <br></br>
-          <div className={style["list-bar"]}>정보</div>
+          <div className={style["list-bar"]}>다른 기능</div>
           <hr></hr>
           <ul className={style.listContainer}>
-            <li
-              className={style.mainComponent}
-              onClick={() => handleRequestURL(requestURL.naverform)}
-            >
-              <img src={fav_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>최애 신청하기(네이버 폼)</p>
+            <li className={style.mainComponent} onClick={() => handleRequestURL(requestURL.naverform)}>
+              <img src={notice} alt="Arrow" className={style.vector} />
+              <p className={style.bodyText}>공지사항</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
             <li className={style.mainComponent} onClick={() => handlePage("/notice_list")}>
-              <img src={noti_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>공지사항</p>
+              <img src={bug_icon} alt="Arrow" className={style.vector} />
+              <p className={style.bodyText}>버그 리포트</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
             <li className={style.mainComponent} onClick={() => handlePage("/terms_page")}>
@@ -223,9 +198,7 @@ function MoreSee({ onModeChange }) {
             </li>
             <li className={style.mainComponent} onClick={handleChangeMode}>
               <img src={set_icon} alt="Arrow" className={style.vector} />
-              <p className={style.bodyText}>
-                {brightMode === "dark" ? "☀️ Light Mode" : "🌑 Dark Mode"}
-              </p>
+              <p className={style.bodyText}>{brightMode === "dark" ? "☀️ Light Mode" : "🌑 Dark Mode"}</p>
               <img src={more_icon} alt="Arrow" className={style.more_vector} />
             </li>
           </ul>
@@ -257,9 +230,7 @@ function MoreSee({ onModeChange }) {
           <p className={style.nova_info}>경북 경산시 압량읍 압독2로1길 21, 1층 184</p>
           <p className={style.nova_info}>대표: 김민관 | 사업자등록번호: 380-08-03011</p>
           {/* <p className="nova-info">통신판매업신고번호: 0000-0000-000000</p> */}
-          <p className={style.nova_info}>
-            전화번호: 010-9875-2508 | 이메일:youths0828@nova-platform.kr
-          </p>
+          <p className={style.nova_info}>전화번호: 010-9875-2508 | 이메일:youths0828@nova-platform.kr</p>
           <p className={style.nova_info}>사업자 정보</p>
         </footer>
       </div>
