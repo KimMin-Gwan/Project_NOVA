7177cc7ce904304710c3fc8385814c57575e1203
김민관 <alsrhks2508@naver.com>
2025-02-10 10:56:13 +0900

    Update core_system_view.py

--- server/src/view/core_system_view.py
+++ server/src/view/core_system_view.py
@@ -475,11 +475,10 @@ class Core_Service_View(Master_View):
         def get_feed_with_community(request:Request, raw_request:dict):
             request_manager = RequestManager()
             
+            pprint(raw_request)
+            
             # 데이터 페이로드에도 bid 리스트를 넣어야됨
             data_payload = CommunityRequest(request=raw_request)
-
-            data = []
-            # pprint(raw_request)
             
             request_manager.try_view_management(data_payload=data_payload, cookies=request.cookies)
 
