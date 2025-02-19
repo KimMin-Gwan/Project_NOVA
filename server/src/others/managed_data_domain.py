bb2248739622b4c9234747d5f106b3205cb2d369
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-02-20 05:02:44 +0900

    Search_engine 테스트 중

--- server/src/others/managed_data_domain.py
+++ server/src/others/managed_data_domain.py
@@ -345,7 +345,7 @@ class ManagedFeedBiasTable:
         elif time_type == "weekly":
             searched_df = searched_df[self.__get_time_diff(target_time=searched_df['date'],target_hour=168, reverse=True)]
 
-
+        pprint(searched_df)
         return searched_df['fid'].tolist()
 
     def find_target_index(self, target_hour=1):
