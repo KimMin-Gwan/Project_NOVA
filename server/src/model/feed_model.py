e2a15d719304ebdfdb939034e15008443d91d09c
김민관 <alsrhks2508@naver.com>
2025-02-25 15:55:10 +0900

    Update feed_model.py

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -836,10 +836,13 @@ class FilteredFeedModel(FeedModel):
                                        num_feed:int=4,
                                        ):
 
+        print(1)
+        
         # 필터링 전 Feeds 들을 가져옵니다.
         # 모든 Feed를 가져온 다음. 게시글을 하나하나씩 쳐내는 방식을 씁니다.
         fid_list = feed_manager.get_all_fids()
 
+        print(2)
         # pprint(category)
         # pprint(fclass)
 
@@ -847,15 +850,19 @@ class FilteredFeedModel(FeedModel):
         #   왜 FClass 부터 먼저 진행하나요? -> 간단한 것부터 먼저 분류합니다.
         #
         fid_list = feed_search_engine.try_filtered_feed_with_option(fid_list=fid_list, option="fclass", keys=[fclass])
+        print(3)
 
         # 2차 필터링 : Category 별 분류를 진행합니다.
         # AD의 경우, 생각중
         fid_list = feed_search_engine.try_filtered_feed_with_option(fid_list=fid_list, option="category", keys=category)
+        print(4)
 
         # 마지막, 분류가 끝이 났으면 페이징을 진행합니다.
         fid_list, self._key = feed_manager.paging_fid_list(fid_list=fid_list, last_index=last_index, page_size=num_feed)
 
+        print(5)
         self._send_data = self._make_feed_data_n_interaction_data(feed_manager=feed_manager, fid_list=fid_list)
+        print(6)
 
         return
 
