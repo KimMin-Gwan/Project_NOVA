958094192bcb127510c14c0e85f30eee63397a05
김민관 <alsrhks2508@naver.com>
2025-02-25 15:47:51 +0900

    Update feed_model.py

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -135,11 +135,16 @@ class FeedModel(BaseModel):
 
     def set_best_feed_with_time(self, feed_search_engine:FeedSearchEngine, feed_manager:FeedManager,
                                 search_type, time_type, last_index=-1, num_feed=4):
+        
+        pprint(1)
         fid_list = feed_search_engine.try_get_feed_in_recent(search_type=search_type, time_type=time_type)
+        pprint(2)
         fid_list, self._key = feed_manager.paging_fid_list(fid_list, last_index=last_index, page_size=num_feed)
 
+        pprint(3)
         self._send_data = self._make_feed_data_n_interaction_data(feed_manager=feed_manager, fid_list=fid_list)
 
+        pprint(4)
         return
 
     # def set_today_best_feed(self, feed_search_engine:FeedSearchEngine, feed_manager,
