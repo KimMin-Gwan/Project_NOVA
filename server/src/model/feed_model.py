c4d7b1f51ca1736a5c1029d1723eb3f0c775172e
김민관 <alsrhks2508@naver.com>
2025-02-27 00:01:35 +0900

    Update feed_model.py

--- server/src/model/feed_model.py
+++ server/src/model/feed_model.py
@@ -137,6 +137,7 @@ class FeedModel(BaseModel):
                                 search_type, time_type, last_index=-1, num_feed=4):
         
         fid_list = feed_search_engine.try_get_feed_in_recent(search_type=search_type, time_type=time_type)
+        pprint(fid_list)
         fid_list, self._key = feed_manager.paging_fid_list(fid_list, last_index=last_index, page_size=num_feed)
 
         self._send_data = self._make_feed_data_n_interaction_data(feed_manager=feed_manager, fid_list=fid_list)
