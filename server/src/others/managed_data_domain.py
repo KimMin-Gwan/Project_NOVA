c4e5737584a0cc8a6f2d33e35c3b79f86454401f
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-03-04 23:33:45 +0900

    Test, time_delta 출력

--- server/src/others/managed_data_domain.py
+++ server/src/others/managed_data_domain.py
@@ -125,7 +125,7 @@ class ManagedFeedBiasTable:
     def __get_time_diff(self, target_time, reference_time=datetime.now(),
                         target_hour=0.5, reverse=False) -> bool:
         time_diff = abs(target_time - reference_time)
-        pprint("시간 차 :" + time_diff)
+        pprint("시간 차 :" + str(time_diff))
         pprint("기준 시 :" + str(timedelta(hours=target_hour)))
 
         # 차이가 2시간 이상인지 확인
