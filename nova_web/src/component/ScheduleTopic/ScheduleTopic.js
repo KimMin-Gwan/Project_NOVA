08c167b89fdde350beba8c586e83a6f3849922c6
SeungJun <yangsj8989@naver.com>
2025-03-05 13:51:41 +0900

    컴포넌트 생성

---
+++ nova_web/src/component/ScheduleTopic/ScheduleTopic.js
@@ -0,0 +1,16 @@
+import "./index.css";
+
+export default function ScheduleTopic() {
+  return (
+    <div className="ScheduleTopic">
+      <div>
+        <h3>title</h3>
+        <div>place</div>
+        <div>tag</div>
+        <div>time</div>
+      </div>
+
+      <div className="bias_img">img</div>
+    </div>
+  );
+}
