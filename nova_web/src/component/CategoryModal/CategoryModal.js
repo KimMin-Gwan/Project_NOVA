e598811af3e8ed4c2b4df933351c0ffabba4604c
betteree <kkne1017@gmail.com>
2025-03-10 11:40:05 +0900

    카테고리 변경 모달창창

--- nova_web/src/component/CategoryModal/CategoryModal.js
+++ nova_web/src/component/CategoryModal/CategoryModal.js
@@ -3,14 +3,24 @@ import Board from "../Board/Board";
 import "./index.css";
 import ModalRectangle from "./../../img/ModalRectangle.png";
 
-export default function CategoryModal({ SetIsOpen, onClickCategory, biasId, board, setBoard }) {
+export default function CategoryModal({
+  SetIsOpen,
+  onClickCategory,
+  biasId,
+  board,
+  setBoard,
+  isOpend,
+}) {
   let [boardData, setBoardData] = useState([]);
   let [isLoading, setIsLoading] = useState(true);
 
   async function fetchBoardData() {
-    await fetch(`https://nova-platform.kr/nova_sub_system/try_get_community_side_box?bid=${biasId}`, {
-      credentials: "include",
-    })
+    await fetch(
+      `https://nova-platform.kr/nova_sub_system/try_get_community_side_box?bid=${biasId}`,
+      {
+        credentials: "include",
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         setBoardData(data.body);
@@ -28,17 +38,25 @@ export default function CategoryModal({ SetIsOpen, onClickCategory, biasId, boar
 
   return (
     <div
-      className="CategoryModal"
+      className={`CategoryModal ${isOpend ? "see" : ""}`}
       onClick={() => {
         onClickCategory();
       }}
     >
-      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+      <div
+        className={`modal-container ${isOpend ? "on" : ""}`}
+        onClick={(e) => e.stopPropagation()}
+      >
         <section className="top-section">
           <img src={ModalRectangle} alt="모달 사각형" />
           <div className="modal-title">주제 이름</div>
         </section>
-        <Board SetIsOpen={SetIsOpen} boardData={boardData} board={board} setBoard={setBoard} />
+        <Board
+          SetIsOpen={SetIsOpen}
+          boardData={boardData}
+          board={board}
+          setBoard={setBoard}
+        />
       </div>
     </div>
   );
