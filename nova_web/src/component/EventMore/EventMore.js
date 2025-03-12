0a1cce48044415c909903a7f5ec3504383410ee1
betteree <kkne1017@gmail.com>
2025-03-12 16:41:45 +0900

    상태관리

--- nova_web/src/component/EventMore/EventMore.js
+++ nova_web/src/component/EventMore/EventMore.js
@@ -27,18 +27,39 @@ export default function EventMore({ closeSchedule, isOpen }) {
     };
   }, [isOpen]);
 
+  // 취소했을 때 모두 취소되어서 변화하도록함
+  useEffect(() => {
+    if (isSelect === 1) {
+      handleReset();
+    }
+  }, [isSelect]);
+
   // 선택하면 배경색 변화하게 해주는 거
   function handleSelect(key) {
-    setSelectBack((prev) => ({
-      ...prev,
-      [key]: prev[key] === "" ? "#F1F7FF" : "",
-    }));
+    setSelectBack((prev) => {
+      const newState = {
+        ...prev,
+        [key]: prev[key] === "" || prev[key] === undefined ? "#F1F7FF" : "",
+        // 처음에 초기 값이 ""이나 undefined여도 눌렀을 때 바로 색이 변하도록 함
+      };
+
+      return newState;
+      // 새로운 값에 할당하여 값을 설정해주어서 상태가 즉시 업데이트 되도록 해준다
+    });
   }
+
+  // 선택한 일정 리셋하기
+  function handleReset() {
+    setSelectBack({});
+  }
+
+  // 일정 모두 추가
+  function handleAllSelect() {}
 
   // 일정 선택 하기, 취소하기 토클
   const selectToggle = () => {
+    // 왜 4냐면 button 배열에 선택됐을 때 텍스트가 배열[4]이기 때문임
     setIsSelect((prev) => (prev === 1 ? 4 : 1));
-    // 왜 3이냐면 button 배열에 선택됐을 때 텍스트가 배열[4]이기 때문임
   };
   return (
     <div
@@ -54,14 +75,16 @@ export default function EventMore({ closeSchedule, isOpen }) {
           <img src={ModalRectangle} alt="모달 사각형" />
         </div>
         <ScheduleBundle />
-        <ScheduleMoreAdd selectToggle={selectToggle} selectText={isSelect} />
+        <ScheduleMoreAdd
+          selectToggle={selectToggle}
+          selectText={isSelect}
+          allSelect={handleAllSelect}
+        />
         {exdata.map((key, index) => (
           <ScheduleEvent
             key={index}
-            toggleClick={() => {
-              handleSelect(key);
-            }}
-            selectBack={selectBack[index]}
+            toggleClick={isSelect === 4 ? () => handleSelect(key) : undefined}
+            selectBack={selectBack[index] || ""}
           />
         ))}
       </section>
