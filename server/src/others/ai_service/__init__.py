7701d358f3fc1056b5b4b4d2550bfe1108f8934c
김민관 <alsrhks2508@naver.com>
2025-03-20 18:46:04 +0900

    fixed untracked files

--- server/src/others/ai_service/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from others.ai_service.agents import *
-from others.ai_service.ai_inventory import *
-from others.ai_service.ai_manager import *
-from others.ai_service.ai_recommand import *
-from others.ai_service.feed_analyzer import *
-from others.ai_service.trend_analyzer import *
-
--- server/src/others/ai_service/trend_analyzer.py
+++
@@ -1,5 +0,0 @@
-
-
-class TrendAnalyzer:
-    pass
-
