3ad4316a7edfdb6f92ba648a7eb635b5fb555404
김민관 <alsrhks2508@naver.com>
2025-03-23 20:11:08 +0900

    Update user_system_view.py

--- server/src/view/user_system_view.py
+++ server/src/view/user_system_view.py
@@ -170,6 +170,7 @@ class User_Service_View(Master_View):
             user_controller = UserController()
             model = user_controller.try_get_user_page(database=self.__database,request=request_manager)
             body_data = model.get_response_form_data(self._head_parser)
+            pprint(body_data)
             response = request_manager.make_json_response(body_data=body_data)
             return response
 
