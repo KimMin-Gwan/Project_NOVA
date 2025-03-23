e304b8efbe0a25e8e81f1a7bdca3c4eca4cd7653
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-03-23 22:59:39 +0900

    테스트

--- server/src/controller/time_table_controller.py
+++ server/src/controller/time_table_controller.py
@@ -59,11 +59,12 @@ class TImeTableController:
     # 내 타임 차트 가지고 오기
     def get_time_chart(self, database:Local_Database, request:RequestManager) -> BaseModel: 
         model = ScheduleChartModel(database=database)
-        
-        model.set_user_with_email(request=request.jwt_payload)
-        # 이건 뭔가 이상한 상황일때 그냥 모델 리턴하는거
-        if not model._set_tuser_with_tuid():
-            return model
+
+        if request.jwt_payload != "":
+            model.set_user_with_email(request=request.jwt_payload)
+            # 이건 뭔가 이상한 상황일때 그냥 모델 리턴하는거
+            if not model._set_tuser_with_tuid():
+                return model
 
         #if model.is_tuser_alive():
         model.set_my_schedule_in_by_day(target_date=request.data_payload.date)
