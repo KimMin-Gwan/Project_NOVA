74c7dc8775a449726d58446ebe2889485044c17e
머홍 <101238645+Wa-MeoHong@users.noreply.github.com>
2025-03-26 10:59:50 +0900

    update time_table_model.py

--- server/src/model/time_table_model.py
+++ server/src/model/time_table_model.py
@@ -1111,7 +1111,7 @@ class MultiScheduleModel(TimeTableModel):
             "key" : self._key
             }
 
-        pprint(body)
+        # pprint(body)
         response = self._get_response_data(head_parser=head_parser, body=body)
         return response
 
