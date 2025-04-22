619888fded68b3042fc95a2995ffd0179c12e314
김민관 <alsrhks2508@naver.com>
2025-04-22 22:05:16 +0900

    Update time_table_view.py

--- server/src/view/time_table_view.py
+++ server/src/view/time_table_view.py
@@ -362,8 +362,6 @@ class TimeTableView(Master_View):
         def try_get_printed_form(request:Request, raw_request:dict):
             request_manager = RequestManager(secret_key=self.__jwt_secret_key)
             
-            pprint(raw_request)
-            
             data_payload = MakeMultipleScheduleRequest(request=raw_request)
             request_manager.try_view_management_need_authorized(data_payload=data_payload, cookies=request.cookies)
 
