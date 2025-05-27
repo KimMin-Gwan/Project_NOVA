55ca006fbfaa3d5ec02caa82c51da23398da0399
김민관 <alsrhks2508@naver.com>
2025-05-27 19:53:08 +0900

    출력 제거

--- server/src/others/managed_data_domain.py
+++ server/src/others/managed_data_domain.py
@@ -1776,11 +1776,10 @@ class ManagedScheduleTable(ManagedTable):
     # 이거 기능을 분리하는게 나을지도 모르겠는데 생각 해볼려고 함
     def search_explore_schedule(self, time_section:int, style:str, category:str, gender:str, return_id:bool):
         tags = []
-        
         #tags.append(style)
         tags.append(category)
         
-        print(tags)
+        # * style을 사용해서 뭔가 필터링하는 부분이 필요하다고 판단됨 
         
         searched_df = self._search_data_with_key_str_n_columns(df=self.__schedule_df, time_section=time_section,
                                                                bias_gender=gender, tags=tags)
--- server/src/view/time_table_view.py
+++ server/src/view/time_table_view.py
@@ -243,8 +243,6 @@ class TimeTableView(Master_View):
             request_manager = RequestManager(secret_key=self.__jwt_secret_key)
             data_payload = ExploreScheduleRequset(request=raw_request)
             
-            data_payload()
-            
             request_manager.try_view_management(data_payload=data_payload, cookies=request.cookies)
 
             time_table_controller =TImeTableController()
