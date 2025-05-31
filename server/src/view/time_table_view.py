abbc34f9ad89d36f77604c7f1aab332b3b4e3302
김민관 <alsrhks2508@naver.com>
2025-05-31 22:14:40 +0900

    Update time_table_view.py

--- server/src/view/time_table_view.py
+++ server/src/view/time_table_view.py
@@ -224,6 +224,9 @@ class TimeTableView(Master_View):
         @self.__app.post('/time_table_server/get_schedule_with_sids')
         def try_search_bias_with_sid(request:Request, raw_request:dict):
             request_manager = RequestManager(secret_key=self.__jwt_secret_key)
+            
+            pprint(raw_request)
+            
             data_payload = GetSchedulesRequest(request=raw_request)
             request_manager.try_view_management(data_payload=data_payload, cookies=request.cookies)
 
