38fd2950ea5f1cb167c284945b7148de9e207227
김민관 <alsrhks2508@naver.com>
2025-06-21 13:23:24 +0900

    로딩 화면 변경 (임시)

--- nova_web/src/pages/LoadingPage/LoadingPage.js
+++ nova_web/src/pages/LoadingPage/LoadingPage.js
@@ -1,10 +1,25 @@
-import { RingLoader } from "react-spinners";
+///import { RingLoader } from "react-spinners";
+///import "./index.css";
+
+///export default function LoadingPage() {
+  ///return (
+    ///<div className="LoadingPage">
+      ///<RingLoader size={100} color="#2863cd" />
+    ///</div>
+  ///);
+///}
+
+
 import "./index.css";
+import loadGIF from "./loading_ani.gif";
 
 export default function LoadingPage() {
   return (
     <div className="LoadingPage">
-      <RingLoader size={100} color="#2863cd" />
+      <img 
+        src={loadGIF} 
+        className="BrandLoader" 
+      />
     </div>
   );
 }
