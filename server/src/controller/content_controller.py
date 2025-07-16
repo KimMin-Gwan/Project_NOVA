aec17c320ad36282ece8196d61d4a6e2a3ae06a6
김민관 <alsrhks2508@naver.com>
2025-07-16 14:55:13 +0900

    Update content_controller.py

--- server/src/controller/content_controller.py
+++ server/src/controller/content_controller.py
@@ -30,14 +30,18 @@ class ContentController:
             "Content-Type": "application/json"
         }
         
+        pprint(requests_data)
         
-        result = requests.post(
-            url="https://openapi.chzzk.naver.com/auth/v1/token",
-            headers=headers,
-            data=requests_data
-        )
+        pprint(headers)
+
         
-        pprint(result.json())
+        #result = requests.post(
+            #url="https://openapi.chzzk.naver.com/auth/v1/token",
+            #headers=headers,
+            #data=requests_data
+        #)
+        
+        #pprint(result.json())
         
         result={
             'accessToken':"temp",
