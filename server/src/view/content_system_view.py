cfdfa5670017a153042508cf0ec939efe19aa31e
김민관 <alsrhks2508@naver.com>
2025-07-17 16:32:02 +0900

    Update content_system_view.py

--- server/src/view/content_system_view.py
+++ server/src/view/content_system_view.py
@@ -35,6 +35,7 @@ class Content_Service_view(Master_View):
         @self.__app.get('/content_system/try_subscribe_chat')
         def get_num_music_content(sessionKey:Optional[str]):
             print(sessionKey)
+            print(type(sessionKey))
 
             content_controller = ContentController()
             data_payload = ChzzkSubscribeRequest(session_key=sessionKey)
