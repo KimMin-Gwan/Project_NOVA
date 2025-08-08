ba69eebbea0dba52bddc9d53a1c751172dcb3f88
김민관 <alsrhks2508@naver.com>
2025-08-08 20:51:55 +0900

    검색창 데스크탑 레이아웃 별도 제작

---
+++ nova_web/src/component/SearchBoxDesktop.jsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import search_icon from "./../img/home_search.svg";
+import style from "./SearchBoxDesktop.module.css";
+
+export default function SearchBoxDesktop({
+  type,
+  searchWord,
+  onClickSearch,
+  onChangeSearchWord,
+  onKeyDown,
+}) {
+  let navigate = useNavigate();
+
+  function onClickSearchBtn(e) {
+    e.stopPropagation();
+    if (type === "search" || onClickSearch) {
+      onClickSearch();
+    }
+  }
+
+  return (
+    <div className={style["search_input_wrapper"]}>
+        <input className={style["search_input"]}
+        value={searchWord}
+        onKeyDown={onKeyDown}
+        onChange={(e) => {
+            onChangeSearchWord(e);
+        }}
+        placeholder="콘텐츠 또는 게시글을 검색 할 수 있어요"
+        type="text"
+        />
+    </div>
+  );
+}
