a34f41067744f078a6ed72a8221eba17d70adaf1
김민관 <alsrhks2508@naver.com>
2025-08-12 17:55:35 +0900

    대부분의 기능을 이곳으로 옮김

--- server/src/model/sub_model.py
+++ server/src/model/sub_model.py
@@ -1,117 +1,25 @@
 from model.base_model import BaseModel
 from model import Mongo_Database
 from others.data_domain import Bias, User, Report
-from others import CoreControllerLogicError, HTMLEXtractor, ImageDescriper, MailSender
-import copy
-from urllib.parse import urlparse
-import datetime
+from others import CoreControllerLogicError, HTMLEXtractor, ImageDescriper, MailSender, FeedSearchEngine
+import uuid
 
-class BiasBannerModel(BaseModel):
+class BannerModel(BaseModel):
     def __init__(self, database:Mongo_Database) -> None:
         super().__init__(database)
+        self.__banner = []
 
-    def get_response_form_data(self, head_parser):
+    def set_banner_data(self):
         try:
-            body = {
-                'result' : []
-            }
-
-            response = self._get_response_data(head_parser=head_parser, body=body)
-            return response
+            self.__banner = self._database.get_all_data(target="banner")
 
         except Exception as e:
-            raise CoreControllerLogicError("response making error | " + e)
+            raise CoreControllerLogicError(error_type="set_group_leagues | " + str(e))
 
-class UserContributionModel(BaseModel):
-    def __init__(self, database:Mongo_Database) -> None:
-        super().__init__(database)
-        self._bias = Bias()  # 목표 선택용 bias
-        self._users = []   # 랭크 집계를 위한 bias 리스트
-
-    def set_bias_data(self, request):
-        bias_data = self._database.get_data_with_id(target="bid", id=request.bid)
-
-        if not bias_data:
-            return False
-
-        self._bias.make_with_dict(dict_data=bias_data)
-        return True
-    
-    # 유저 정보 받아오기
-    def set_user_datas(self):
-
-
-        # solo_bid 같은건 없어졌는데 왜 여기 있음?
-
-        if self._bias.type == "solo":
-            user_datas = self._database.get_datas_with_key(target="uid", key="solo_bid", key_datas=[self._bias.bid])
-        elif self._bias.type == "group":
-            user_datas = self._database.get_datas_with_key(target="uid", key="group_bid", key_datas=[self._bias.bid])
-        else:
-            return False
-        
-        for user_data in user_datas:
-            user = User()
-            user.make_with_dict(dict_data=user_data)
-            self._users.append(user)
-
-        return True
-
-    def set_user_alignment(self):
-        if self._bias.type == "solo":
-            self._users = sorted(self._users, key=lambda x: x.solo_point, reverse=True)
-        elif self._bias.type == "group":
-            self._users = sorted(self._users, key=lambda x: x.group_point, reverse=True)
-        else:
-            return False
-        
-        return True
-    
-    def _get_dict_user_data_with_rank(self):
-        dict_user_form_list = []
-        priv_point = 10000
-        now_rank = 0
-        set_rank = 0 
-
-        if self._bias.type == "solo":
-            for user in self._users:
-                now_point = user.solo_point
-                if now_point < priv_point:
-                    now_rank = now_rank + set_rank + 1
-                    set_rank = 0
-                    priv_point = now_point
-                elif now_point == priv_point:
-                    set_rank += 1
-                else:
-                    print("data align set badly")
-                user_data = user.get_dict_form_data()
-                user_data['point'] = user.solo_point
-                user_data['rank'] = now_rank
-                dict_user_form_list.append(user_data)
-        elif self._bias.type == "group":
-            for user in self._users:
-                now_point = user.group_point
-                if now_point < priv_point:
-                    now_rank = now_rank + set_rank + 1
-                    set_rank = 0
-                    priv_point = now_point
-                elif now_point == priv_point:
-                    set_rank += 1
-                else:
-                    print("data align set badly")
-                user_data = user.get_dict_form_data()
-                user_data['point'] = user.group_point
-                user_data['rank'] = now_rank
-                dict_user_form_list.append(user_data)
-        else:
-            print("bias_type error")
-
-        return dict_user_form_list
-    
     def get_response_form_data(self, head_parser):
         try:
             body = {
-                'user_contribution' : self._get_dict_user_data_with_rank(),
+                'banner' : self.__banner
             }
 
             response = self._get_response_data(head_parser=head_parser, body=body)
@@ -120,49 +28,7 @@ class UserContributionModel(BaseModel):
         except Exception as e:
             raise CoreControllerLogicError("response making error | " + e)
         
-class MyContributionModel(UserContributionModel):
-    def __init__(self, database:Mongo_Database) -> None:
-        super().__init__(database)
-        self.__result = False  # 내가 팔로우 중이면 True
-
-    # 내 최애가 맞는지 확인
-    def is_my_bias(self) -> bool:
-        if self._bias.bid in self._user.bids:
-            self.__result = True
-            return True
-        else:
-            return False
-
-    # rank와 point를 포함한 데이터
-    def _get_my_data(self) -> dict:
-        user_data = self._get_dict_user_data_with_rank()
-        point = 0
-        rank = 0
-
-        for user in user_data:
-            if user['uid'] == self._user.uid:
-                point = user['point']
-                rank = user['rank']
-
-        dict_user = self._user.get_dict_form_data()
-        dict_user['point'] = point
-        dict_user['rank'] = rank
-        return dict_user
-
-    def get_response_form_data(self, head_parser):
-        try:
-            body = {
-                'my_contribution' : self._get_my_data(),
-                'result' : self.__result
-            }
-
-            response = self._get_response_data(head_parser=head_parser, body=body)
-            return response
-
-        except Exception as e:
-            raise CoreControllerLogicError("response making error | " + e)
-
-
+        
 class ImageTagModel(BaseModel):
     def __init__(self, database:Mongo_Database) -> None:
         super().__init__(database)
@@ -185,87 +51,6 @@ class ImageTagModel(BaseModel):
 
         except Exception as e:
             raise CoreControllerLogicError("response making error | " + e)
-
-
-class CommunitySideBoxModel(BaseModel):
-    def __init__(self, database):
-        super().__init__(database)
-        self.__bias = Bias()
-        self.__urls = {}
-        self._boards = []
-
-    def get_response_form_data(self, head_parser):
-        try:
-            body = {
-                'bname' : self.__bias.bname,
-                'urls' : self.__urls,
-                'boards' : self._boards
-            }
-
-            response = self._get_response_data(head_parser=head_parser, body=body)
-            return response
-
-        except Exception as e:
-            raise CoreControllerLogicError("response making error | " + e)
-
-    def get_boards_of_bias_community(self, bid):
-        bias_data = self._database.get_data_with_id(target='bid', id=bid)
-        
-        if bias_data:
-            self.__bias.make_with_dict(bias_data)
-            self._boards = copy.copy(self.__bias.board_types)
-            return True
-        else:
-            return False
-
-    def get_urls_of_bias(self):
-        # URL 패턴. 고정된 패턴들을 포함합니다.
-        self.__urls["X"] = self.__bias.x_account
-        self.__urls["Instagram"] =  self.__bias.insta_account
-        self.__urls["TikTok"]=  self.__bias.tiktok_account
-        self.__urls["Youtube"] = self.__bias.youtube_account
-
-        # 홈페이지 패턴, 주요한 곳들을 추가했음. 팝콘, 팬더는 일부러 안넣음 만약 넣어야한다면 넣겠다.
-
-        # 치지직
-        if "chzzk" in self.__bias.homepage:
-            self.__urls["Chzzk"] = self.__bias.homepage
-        # SOOP, 구 아프리카티비
-        elif "soop" in self.__bias.homepage or "afreecatv" in self.__bias.homepage:
-            self.__urls["SOOP"] = self.__bias.homepage
-        # 플렉스 티비. 여기 여캠 비중 압도적인 신생 플랫폼
-        elif "flextv" in self.__bias.homepage:
-            self.__urls["FlexTV"] = self.__bias.homepage
-        # 카카오티비. 구 다음팟 티비
-        elif "kakao" in self.__bias.homepage:
-            self.__urls["KakaoTV"] = self.__bias.homepage
-        # 트위치. 망했지만 넣긴하는게 맞음. 해외시청자가 주인 방송인들은 아직 잔류하는 듯
-        elif "twitch" in self.__bias.homepage:
-            self.__urls["Twitch"] = self.__bias.homepage
-
-        # 나머지 플랫폼 : Default로 두겠음
-        else:
-            self.__urls["Default"] = self.__bias.homepage
-
-        # 팬카페 패턴
-        if "naver" in self.__bias.fan_cafe:
-            self.__urls["Naver"] = self.__bias.fan_cafe
-        elif "daum" in self.__bias.fan_cafe:
-            self.__urls["Daum"] = self.__bias.fan_cafe
-
-        # 디코로 대화하는 쪽도 있어서 걍 넣음
-        elif "discord" in self.__bias.fan_cafe:
-            self.__urls["Discord"] = self.__bias.fan_cafe
-
-        # 팬카페는 3가지 경우에 대해서만 하겠음
-        # 팬심은 안넣습니다.
-        else:
-            self.__urls["Default"] = self.__bias.fan_cafe
-
-        # 마지막. 키 순회하면서 ""인 값들을 모두 쳐냄
-        for key in list(self.__urls.keys()):
-            if self.__urls[key] == "":
-                del self.__urls[key]
 
 
 class ReportModel(BaseModel):
@@ -297,7 +82,6 @@ class ReportModel(BaseModel):
                                                     image_names=data_payload.image_names,
                                                     images=data_payload.images
                                                     )
-        
         return
     
     # 리포트 저장
@@ -318,45 +102,7 @@ class ReportModel(BaseModel):
         except Exception as e:
             raise CoreControllerLogicError("response making error | " + e)
 
-class ChangeUserAgeModel(BaseModel):
-    def __init__(self, database:Mongo_Database) -> None:
-        super().__init__(database)
-        self._result = False
-        self._detail = ""
-
-    def get_response_form_data(self, head_parser):
-        try:
-            body = {
-                'result' : self._result,
-                'detail' : self._detail
-            }
-
-            response = self._get_response_data(head_parser=head_parser, body=body)
-            return response
-
-        except Exception as e:
-            raise CoreControllerLogicError("response making error | " + e)
-
-    def change_users_age(self):
-        today = datetime.datetime.now()
-        user_datas = self._database.get_all_data(target="uid")
-        if today.month == 1 and today.day == 1:
-            for user_data in user_datas:
-                user_data['age'] += 1
-
-        # 하나하나 저장 함.
-        for user_data in user_datas:
-            self._database.modify_data_with_id(target_id="uid", target_data=user_data)
-
-        self._result = True
-        self._detail = "Complete Change all users age"
-
-        return
-
-
-
 # bias.state => "DEFAULT", "TEMP", "CONFIRMED", "APPROVED"
-import uuid
 
 class MakeNewBiasModel(BaseModel):
     def __init__(self, database:Mongo_Database) -> None:
@@ -400,11 +146,194 @@ class MakeNewBiasModel(BaseModel):
         self._database.add_new_data(target_id="bid", new_data=self._bias.get_dict_form_data())
         self._result = True
         return
+    
+class MyBiasModel(BaseModel):
+    def __init__(self, database:Mongo_Database) -> None:
+        super().__init__(database)
+        self.__bias_list = []
+        
+    def get_bid_list(self):
+        bid_list = []
+        for bias in self.__bias_list:
+            bid_list.append(bias.bid)
+        return bid_list
+    
+    def get_bias_list(self):
+        return self.__bias_list
 
+    # 바이어스 데이터를 모두 받아와서 만들기
+    def set_bias_list(self):
+        if self._user.bids:
+            bias_datas = self._database.get_datas_with_ids(target_id="bid", ids=self._user.bids)
+
+            for bias_data in bias_datas:
+                bias = Bias()
+                bias.make_with_dict(bias_data)
+                self.__bias_list.append(bias)
+        return
         
+
+    def get_response_form_data(self, head_parser):
+        try:
+            body = {
+                "bias_list" : self._make_dict_list_data(list_data=self.__bias_list)
+            }
+
+            response = self._get_response_data(head_parser=head_parser, body=body)
+            return response
+
+        except Exception as e:
+            raise CoreControllerLogicError("response making error | " + e)
+    
+class BiasFollowModel(BaseModel):
+    def __init__(self, database:Mongo_Database) -> None:
+        super().__init__(database)
+        self.__bias= Bias()
+        self.__result = False
+
+    def find_bias(self, bid):
+        try:
+            raw_bias_data = self._database.get_data_with_id(target="bid", id=bid)
+            if not raw_bias_data:
+                return False
+            self.__bias.make_with_dict(raw_bias_data)
+
+        except Exception as e:
+            raise CoreControllerLogicError(error_type="find_bias | " + str(e))
+        return True
+    
+    # 내 최애를 누구로 할지 최종 결정하는 부분
+    def set_my_bias(self, feed_search_engine:FeedSearchEngine):
+        if self.__bias.bid in self._user.bids:
+            self._user.bids.remove(self.__bias.bid)
+            feed_search_engine.remove_user_to_bias(bid=self.__bias.bid, uid=self._user.uid)
+        else:
+            self._user.bids.append(self.__bias.bid)
+            feed_search_engine.add_new_user_to_bias(bid=self.__bias.bid, uid=self._user.uid)
+
+        self._database.modify_data_with_id(target_id="uid", target_data=self._user.get_dict_form_data())
+        self.__result = True
+
+        return True
+
+
+    def get_response_form_data(self, head_parser):
+        try:
+            body = {
+                'result' : self.__result
+            }
+
+            response = self._get_response_data(head_parser=head_parser, body=body)
+            return response
+
+        except Exception as e:
+            raise CoreControllerLogicError("response making error | " + e)
+
+class BiasSearchModel(BaseModel):
+    def __init__(self, database:Mongo_Database) -> None:
+        super().__init__(database)
+        self._biases = []
         
+    # 바이어스 전체 데이터 세팅
+    def set_biases(self):
+        bias_datas = self._database.get_all_data(target="bias")
         
+        for bias_data in bias_datas:
+            bias = Bias()
+            bias.make_with_dict(dict_data=bias_data)
+            self._biases.append(bias)
+            
+        if len(self._biases):
+            return True
+        else:
+            return False
+            
+    
+    def try_search_bias_with_category(self, category:str, biases=[]):
+        self._biases=self._try_search_bias_with_category(category=category, biases=biases)
+        return
         
+    # 카테고리를 바탕으로 검색 -> 반드시 set_biases가 선행해도됨
+    def _try_search_bias_with_category(self, category:str, biases=[]):
+        result = []
+        
+        if len(biases):
+            for bias in biases:
+                if category in bias.platform:
+                    result.append(bias)
+        else:
+            for bias in self._biases:
+                if category in bias.platform:
+                    result.append(bias)
+                    
+        return result
+
+    def try_search_bias(self, bname, feed_search_engine:FeedSearchEngine):
+        # ManagedBias 전체 호출
+        managed_bias_list = feed_search_engine.get_all_managed_bias()
+
+        # 코사인 유사도 평가를 통한 가장 비슷한 이름 검색
+        result:list = self._search_similar_data(data_list=managed_bias_list,
+                                    key_word=bname, key_attr="bname")
+            
+        # 검색 결과가 있으면 데이터 베이스에서 찾아서 보내줌
+        if result:
+            bids = []
+                
+            # ManagedBias에서 bid 추출
+            for managed_bias in result:
+                bids.append(managed_bias.bid)
+                    
+            # 검색 후 만들기
+            bias_datas = self._database.get_datas_with_ids(target_id="bid", ids=bids)
+            for bias_data in bias_datas:
+                bias = Bias()
+                bias.make_with_dict(bias_data)
+                self._biases.append(bias)
+        return
+
+    def get_response_form_data(self, head_parser):
+        try:
+            body = {
+                'biases' : self._make_dict_list_data(list_data=self._biases)
+            }
+
+            response = self._get_response_data(head_parser=head_parser, body=body)
+            return response
+
+        except Exception as e:
+            raise CoreControllerLogicError("response making error | " + e)
+        
+class BiasFollowPageModel(BiasSearchModel):
+    def __init__(self, database:Mongo_Database) -> None:
+        super().__init__(database)
+        self.__chzzk= []
+        self.__soop= []
+        self.__artist= []
+        self.__youtube= []
+        
+    def try_get_bias_follow_page_data(self):
+        self.__chzzk = self._try_search_bias_with_category(category="치지직")
+        self.__soop = self._try_search_bias_with_category(category="SOOP")
+        self.__artist= self._try_search_bias_with_category(category="아티스트")
+        self.__youtube= self._try_search_bias_with_category(category="크리에이터")
+        return
+
+
+    def get_response_form_data(self, head_parser):
+        try:
+            body = {
+                'chzzk' : self._make_dict_list_data(list_data=self.__chzzk),
+                'soop' : self._make_dict_list_data(list_data=self.__soop),
+                'artist' : self._make_dict_list_data(list_data=self.__artist),
+                'youtube' : self._make_dict_list_data(list_data=self.__youtube)
+            }
+
+            response = self._get_response_data(head_parser=head_parser, body=body)
+            return response
+
+        except Exception as e:
+            raise CoreControllerLogicError("response making error | " + e)
         
         
         
