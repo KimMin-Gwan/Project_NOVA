06cb922db54d0ef78364aded07161683689396ad
김민관 <alsrhks2508@naver.com>
2025-08-12 13:35:10 +0900

    Update __init__.py

--- server/src/model/__init__.py
+++ server/src/model/__init__.py
@@ -1,7 +1,5 @@
 from model.local_database_model import *
 from model.base_model import *
-from model.request_login_model import *
-from model.league_model import *
 from model.home_model import *
 from model.user_models import *
 from model.check_model import *
