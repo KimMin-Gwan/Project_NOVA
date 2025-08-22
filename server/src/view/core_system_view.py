f07a5f3c76189fcb238fcb4afaec0f4aab01279d
김민관 <alsrhks2508@naver.com>
2025-08-22 16:36:23 +0900

    Update core_system_view.py

--- server/src/view/core_system_view.py
+++ server/src/view/core_system_view.py
@@ -264,9 +264,8 @@ class Core_Service_View(Master_View):
 
             feed_controller =Feed_Controller(feed_manager=self.__feed_manager)
             model = feed_controller.try_edit_feed(database=self.__database,
-                                                        request=request_manager,
-                                                        feed_manager=self.__feed_manager,
-                                                        )
+                                                request=request_manager,
+                                                feed_manager=self.__feed_manager)
             body_data = model.get_response_form_data(self._head_parser)
             response = request_manager.make_json_response(body_data=body_data)
             return response
