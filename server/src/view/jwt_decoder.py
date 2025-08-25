a56e771cc452beb0e3e7b6b3d20c949512910f71
김민관 <alsrhks2508@naver.com>
2025-08-25 21:01:01 +0900

    Update jwt_decoder.py

--- server/src/view/jwt_decoder.py
+++ server/src/view/jwt_decoder.py
@@ -260,6 +260,8 @@ class RequestManager(JWTManager):
         
     # json데이터 보내줘야할 때 response 만드는 곳
     def make_json_response_with_hmac(self, body_data:dict):
+        pprint(body_data)
+        
         new_token = HMACManger(secret_key=self._secret_key).sign_count(body_data.get("count", 0))
 
         pprint(new_token)
