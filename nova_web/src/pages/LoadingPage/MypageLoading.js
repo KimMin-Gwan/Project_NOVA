3bd5ecda7eca39d72d996eeadf6d15f441a301ea
김민관 <alsrhks2508@naver.com>
2025-08-27 19:39:23 +0900

    로딩 변경
    이제 점3개는 없음

--- nova_web/src/pages/LoadingPage/MypageLoading.js
+++ nova_web/src/pages/LoadingPage/MypageLoading.js
@@ -1,10 +1,16 @@
-import { BeatLoader } from "react-spinners";
+//import { BeatLoader } from "react-spinners";
+//<BeatLoader size={20} color="#2863cd" />
+
 import "./index.css";
+import loading_ani2 from "./loading_ani2.gif";
+
 
 export default function MyPageLoading() {
   return (
     <div className="MyPageLoading">
-      <BeatLoader size={20} color="#2863cd" />
+      <img src={loading_ani2}
+        className="DefaultLoader"
+      />
     </div>
   );
 }
