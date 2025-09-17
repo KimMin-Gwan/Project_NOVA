d14abfb04e8f9769eeaa930a37bb9a34b2fa0ddd
김민관 <alsrhks2508@naver.com>
2025-09-17 20:06:50 +0900

    Update time_table_controller.py

--- server/src/controller/time_table_controller.py
+++ server/src/controller/time_table_controller.py
@@ -69,7 +69,7 @@ class TimeTableController:
         else:
             return model
 
-        model.add_schedule(sid=request.data_payload.sid)
+        model.handle_subscribe_schedule(sid=request.data_payload.sid)
         return model
 
 
