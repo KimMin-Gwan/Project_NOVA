8c1358a3dc4bed0198a7e52f6226ee51ad084b0e
김민관 <alsrhks2508@naver.com>
2025-09-20 22:56:17 +0900

    콘텐츠 일정 자세히 보기 모달창 완성
    아직 위치에 맞춰서 나오는건 안되서 그건 별도로 구성해야함

--- nova_web/src/component/ScheduleDetail/ScheduleDetailDesktop.jsx
+++ nova_web/src/component/ScheduleDetail/ScheduleDetailDesktop.jsx
@@ -1,60 +1,166 @@
 import React, { useRef, useEffect, useState } from "react";
-
 import style from "./ScheduleDetailDesktop.module.css";
+import { handlePreviewImage, formatKoreanDateTime, fetchScheduleData, fetchSubscribeSchedule } from "./ScheduleDetailFunction";
+import { SCHEDULE_IMAGE_URL } from "../../constant/imageUrl";
+import { useNavigate } from "react-router-dom";
+import MyPageLoading from "../../pages/LoadingPage/MypageLoading";
 
-const ScheduleDetailDekstop = () => {
-    return(
-        <div className={style["modal-frame"]}>
+const ScheduleDetailDekstop = ({sid, toggleMoreOption}) => {
+    const url = "";
+    const [image, setImage] = useState(null);
+    const [tags, setTags] = useState([]);
+    const [schedule, setSchedule] = useState({});
+    const [subscribe, setSubscribe] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const datetime = new Date(schedule.datetime);
+
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const schedule = await fetchScheduleData(sid); // ✅ 데이터 기다림
+                if (!schedule || !schedule.sid) {
+                    alert("콘텐츠 일정을 불러오는데 문제가 있습니다.");
+                    toggleMoreOption(null);
+                    return;
+                }
+                
+                setSchedule(schedule);
+                setTags(schedule.tags);
+                setSubscribe(schedule.subscribe);
+
+                const url = `${SCHEDULE_IMAGE_URL}${schedule.sid}.png`;
+                handlePreviewImage(url, setImage);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("일정 불러오기 실패:", error);
+                alert("일정을 불러오는 중 오류가 발생했습니다.");
+                toggleMoreOption(null);
+            }
+        };
+
+        fetchData();
+    },[])
+
+    const { dateStr, timeStr } = formatKoreanDateTime(datetime);
+
+    if (isLoading){
+        <div className={style["modal-frame"]}
+            onClick={()=>{toggleMoreOption(false);}}
+        >
             <div className={style["modal-container"]}>
-                <div className={style["modal-upper-section"]}>
-                </div>
-                <div className={style["modal-lower-section"]}>
-                    <div className={style["schedule-tag-detail-wrapper"]}>
-                        <div className={style["tag-wrapper-wrapper"]}>
-                            <div className={style["tag-wrapper"]}>
-                                <div className={style["tag-box"]}>
-                                    태그
-                                </div>
-                                <div className={style["tag-box"]}>
-                                    태그
-                                </div>
-                                <div className={style["platform-box"]}>
-                                    치지직
-                                </div>
-                            </div>
-                            <div className={style["schedule-bname"]}>
-                                스트리머 이름
-                            </div>
-                        </div>
-                        <div className={style["schedule-detail-wrapper"]}>
-                            <div className={style["schedule-title"]}>
-                                콘텐츠 일정 이름
-                            </div>
-                            <div className={style["schedule-datetime"]}>
-                                <div>
-                                    2025년 09월 18일 토요일
-                                </div>
-                                <div>
-                                    |
-                                </div>
-                                <div>
-                                    오후 06시 50분
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={style["button-wrapper"]}>
-                        <div className={style["left-button"]}>
-                            플랫폼 바로가기
-                        </div>
-                        <div className={style["right-button"]}>
-                            지금 콘텐츠 구독
-                        </div>
-                    </div>
-                </div>
+                <MyPageLoading/>
             </div>
         </div>
-    );
+    }else{
+        return(
+            <div className={style["modal-frame"]}
+                onClick={()=>{toggleMoreOption(null);}}
+            >
+                <div className={style["modal-container"]}>
+                    {
+                        isLoading ?  <MyPageLoading/> 
+                        :
+                        <>
+                            <div className={style["modal-upper-section"]}>
+                            {
+                                image &&
+                                <div className={style["schedule-image"]}>
+                                    <img src={image} alt="스케줄 이미지" />
+                                </div>
+                            }   
+                            </div>
+                            <div className={style["modal-lower-section"]}>
+                                <div className={style["schedule-tag-detail-wrapper"]}>
+                                    <div className={style["tag-wrapper-wrapper"]}>
+                                        <div className={style["tag-wrapper"]}>
+                                            {
+                                                tags.map((tag, i)=>{
+                                                    return(
+                                                        <div className={style["tag-box"]}>
+                                                            {tag}
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                            {
+                                                schedule.platform &&
+                                                <div className={style["platform-box"]}>
+                                                    {schedule.platform}
+                                                </div>
+                                            }
+                                        </div>
+                                        <div className={style["schedule-bname"]}>
+                                            {schedule.bname}
+                                        </div>
+                                    </div>
+                                    <div className={style["schedule-detail-wrapper"]}>
+                                        <div className={style["schedule-title"]}>
+                                            {schedule.title}
+                                        </div>
+                                        <div className={style["schedule-datetime"]}>
+                                            <div>
+                                                {dateStr}
+                                            </div>
+                                            <div>
+                                                |
+                                            </div>
+                                            <div>
+                                                {timeStr}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={style["button-wrapper"]}>
+                                    <div className={style["left-button"]}
+                                        onClick={() => {
+                                            if (schedule.url != "https://supernova.io.kr"){
+                                                window.open(schedule.url, "_blank")
+                                            } else{
+                                                if (schedule.platform == "치지직"){
+                                                    window.open(`https://chzzk.naver.com/search?query=${schedule.bname}`, "_blank")
+                                                }else{
+                                                    window.open(`https://www.sooplive.co.kr/search?szLocation=total_search&szSearchType=total&szKeyword=${schedule.bname}&szStype=di&szActype=input_field`, "_blank")
+                                                }
+                                            }
+                                        }}
+                                    >
+                                        플랫폼 바로가기
+                                    </div>
+                                    {
+                                    schedule.is_owner ? (
+                                        <div className={style["right-button"]}
+                                            onClick={()=>{
+                                                navigate(`/schedule/make_new/${schedule.sid}`)
+                                            }}
+                                        >콘텐츠 수정</div>
+                                    ) : (
+                                        subscribe ? (
+                                        <div className={style["right-button"]}
+                                            onClick={()=>{
+                                                fetchSubscribeSchedule(schedule.sid);
+                                                setSubscribe(false);
+                                            }}
+                                        >구독 취소</div>
+                                        ) : (
+                                        <div className={style["right-button"]}
+                                            onClick={()=>{
+                                                fetchSubscribeSchedule(schedule.sid);
+                                                setSubscribe(true);
+                                            }}
+                                        >콘텐츠 구독</div>
+                                        )
+                                    )
+                                    }
+                                </div>
+                            </div>
+                        </>
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 
---
+++ nova_web/src/component/ScheduleDetail/ScheduleDetailFunction.jsx
@@ -0,0 +1,63 @@
+import HEADER from "../../constant/header";
+import mainApi from "../../services/apis/mainApi";
+import postApi from "../../services/apis/postApi";
+
+export const handlePreviewImage = (url, setImage) => {
+    const cacheBuster = Date.now(); // 캐시 방지용
+    const urlWithCacheBuster = `${url}?cb=${cacheBuster}`;
+
+    const img = new Image();
+    img.src = urlWithCacheBuster;
+
+    img.onload = () => {
+        setImage(urlWithCacheBuster); // 캐시 방지 URL로 상태 세팅
+    };
+
+    img.onerror = () => {
+        setImage(null);
+    };
+};
+
+export const formatKoreanDateTime = (date) => {
+  if (!(date instanceof Date)) {
+    return "";
+  }
+
+  // 요일 배열
+  const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // 0부터 시작
+  const day = date.getDate();
+  const dayOfWeek = days[date.getDay()];
+
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  // 오전/오후 판별
+  const ampm = hours >= 12 ? "오후" : "오전";
+  hours = hours % 12 || 12; // 12시간제로 변환 (0시는 12시로)
+
+  const dateStr = `${year}년 ${month.toString().padStart(2, "0")}월 ${day.toString().padStart(2, "0")}일 ${dayOfWeek}`;
+  const timeStr = `${ampm} ${hours.toString().padStart(2, "0")}시 ${minutes}분`;
+
+  return { dateStr, timeStr };
+}
+
+export const fetchScheduleData = async (sid) => {
+  const res = await postApi.post('/time_table_server/get_schedule_with_sids', {
+    header: HEADER,
+    body: {
+      sids: [sid]
+    },
+  });
+
+  if (res.data.body.schedules[0]) return res.data.body.schedules[0]
+  else return { sid: "" }
+}
+
+export const fetchSubscribeSchedule = async (sid) => {
+  const res = await mainApi.post(`/time_table_server/try_add_schedule?sid=${sid}`)
+
+  return
+}
