5caa0190aef00c52b55f72e81899bbfd68f39630
김민관 <alsrhks2508@naver.com>
2025-09-21 18:47:40 +0900

    Update managed_data_domain.py

--- server/src/others/managed_data_domain.py
+++ server/src/others/managed_data_domain.py
@@ -1190,9 +1190,9 @@ class ManagedScheduleTable(ManagedTable):
         # * style을 사용해서 뭔가 필터링하는 부분이 필요하다고 판단됨 
         searched_df = self._search_data_with_key_str_n_columns(df=self.__schedule_df, time_section=time_section,
                                                                bias_gender=gender, tags=tags)
-        print(searched_df)
         
         searched_df = self._filter_data_with_date_option(df=searched_df, date_option="weekly", date_columns=["datetime"])
+        print(searched_df)
         
         if searched_df.empty:
             logging.warning(f"Search explore schedule with time_section {time_section}, style {style}, category {category}")
