02caaf5b13127d8bedbe5db53a418a462c5ff132
김민관 <alsrhks2508@naver.com>
2025-09-21 18:23:29 +0900

    검색과 탐색에서 사용될 콘텐츠 일정 컴포넌트(모바일) 제작 완료
    외부 padding 4% -> 부모 width가 500에서 20px로 최대

---
+++ nova_web/src/pages/ScheduleExplore/ScheduleComponentMobile.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import style from "./ScheduleComponentMobile.module.css";
+import { formatDateTime, fecthSubScribeSchedule} from "./ScheduleComponentFunc";
+
+
+const ScheduleComponentMobile = ({
+    toggleMoreOption,
+    title,
+    sid,
+    uname,
+    update_time,
+    bname,
+    datetime,
+    platform,
+    code,
+    toggleClick,
+    selectBack,
+    subscribe
+}) => {
+
+    const { formattedDate, formattedTime } = formatDateTime(datetime);
+    const [isClicked, setIsClicked] = useState(false);
+    const [isSubscribe, setIsSubscribe] = useState(subscribe);
+
+    return (
+        <div className={style["schedule-component-wrapper"]}>
+            <div className={style["schedule-component-frame"]}
+              onClick={()=>{setIsClicked(!isClicked)}}
+              style={{borderRadius : isClicked ? "12px 12px 0px 0px" : "12px 12px 12px 12px"}}
+            >
+                <div className={style["schedule-component-detail-wrapper"]}>
+                    <div className={style["schedule-component-detail"]}>
+                        <div className={style["schedule-title"]}> {title} </div>
+                        <div className={style["schedule-bname"]}> {bname} </div>
+                        <div className={style["schedule-datetime-wrapper"]}>
+                            <div className={style["schedule-datetime"]}>
+                                {formattedDate} | {formattedTime}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style["schedule-platform"]}> {platform} </div>
+                </div>
+            </div>
+            {
+                isClicked && 
+                <div className={style["schedule-button-wrapper"]}>
+                    <div className={style["schedule-button-left"]}
+                        onClick={()=>{toggleMoreOption(sid)}}
+                    >
+                        자세히
+                    </div>
+                    {
+                        isSubscribe ? 
+                        <div className={style["schedule-button-right"]}
+                            onClick={()=>{
+                              if (fecthSubScribeSchedule(sid)){
+                                setIsSubscribe(!isSubscribe)
+                              }}}
+                        >
+                            구독취소
+                        </div>
+                        :
+                        <div className={style["schedule-button-right"]}
+                            onClick={()=>{
+                              if (fecthSubScribeSchedule(sid)){
+                                setIsSubscribe(!isSubscribe)
+                              }
+                            }}
+                        >
+                            구독하기
+                        </div>
+                    }
+                </div>
+            }
+        </div>
+    );
+}
+
+export default ScheduleComponentMobile
