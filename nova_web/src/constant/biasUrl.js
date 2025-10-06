65a51b427e6ce3a25eb8b75be6f23b43b8f82d34
김민관 <alsrhks2508@naver.com>
2025-10-06 21:32:21 +0900

    Update biasUrl.js

--- nova_web/src/constant/biasUrl.js
+++ nova_web/src/constant/biasUrl.js
@@ -1,3 +1,4 @@
 export const BIAS_URL = "https://kr.object.ncloudstorage.com/nova-images/";
+export const DEFAULT_BIAS_URL = "https://kr.object.ncloudstorage.com/nova-images/default_bias.png";
 
 export const REQUEST_URL = "https://naver.me/xGImCJSN";
