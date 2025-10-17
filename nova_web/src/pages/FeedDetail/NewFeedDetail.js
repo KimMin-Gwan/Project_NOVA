9217ead41de84ed881f051baf9ef548518bfba97
김민관 <alsrhks2508@naver.com>
2025-10-17 17:49:42 +0900

    Update NewFeedDetail.js

--- nova_web/src/pages/FeedDetail/NewFeedDetail.js
+++ nova_web/src/pages/FeedDetail/NewFeedDetail.js
@@ -175,7 +175,6 @@ export default function NewFeedDetail() {
     // 데이터를 줄바꿈 단위로 분리
     const [type, owner, uid, uname, cid, body, date] = data.split('<br>');
 
-
     let booleanValue = owner.toLowerCase() === "true";
 
     // 객체 생성 및 반환
@@ -190,7 +189,6 @@ export default function NewFeedDetail() {
       date: date, // 날짜
     };
   }
-
 
   const tryDeleteComment = (cid) => {
     if (socketRef.current) {
@@ -566,6 +564,8 @@ function CommentComponent({cid, uid, owner, uname, isReply, reply, body, date, c
     clearTimeout(pressTimer.current); // 타이머 초기화
   };
 
+  console.log(body, cid);
+
   if(owner){
     return(
       <div id={cid} className={style["comment-component-my"]}>
@@ -640,72 +640,6 @@ function CommentComponent({cid, uid, owner, uname, isReply, reply, body, date, c
   }
 }
 
-
-
-
-// 대댓글
-function ReplyComment({ index, length, reply, fetchOriginalComment, handleRemove }) {
-  const [firstWord, ...restWords] = reply.body.split(" ");
-
-  let src;
-
-  if (length === 1) {
-    src = reArrow1;
-  } else {
-    if (index === 0) {
-      src = reArrow2;
-    } else if (index + 1 === length) {
-      src = reArrow4;
-    } else {
-      src = reArrow3;
-    }
-  }
-
-  return (
-    <div className={style["img-container"]}>
-      <img src={src} alt="대댓글" />
-      <div key={reply.cid} className={`${style["reply-box"]}`} onClick={(e) => e.stopPropagation()}>
-        <div className={style["comment-user"]}>
-          <p>{reply.uname}</p>
-          <div className={style["function_button_container"]}>
-            {reply.is_reworked && (
-              <button
-                className={style["AI_text"]}
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  fetchOriginalComment(reply.cid);
-                }}
-              >
-                원문 보기
-              </button>
-            )}
-            {reply.owner ? (
-              <div
-                className={style["comment-delete-report"]}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleRemove(reply.cid);
-                }}
-              >
-                삭제
-              </div>
-            ) : (
-              <div className={style["comment-delete-report"]}>신고</div>
-            )}
-          </div>
-        </div>
-
-        <div className={style["comment-content"]}>
-          <span style={{ color: reply.mention ? "#2C59CD" : "black" }}>{firstWord} </span>
-          {restWords.join("")}
-        </div>
-
-        <span className={style["date-st"]}> {reply.date}</span>
-      </div>
-    </div>
-  );
-}
 
 // 모달
 function OptionModal({ onClickOption, onClickDelete, fid }) {
