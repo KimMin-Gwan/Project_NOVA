76aa616fba609ef3e34cf0d2edf93a3ccbbdc974
머홍 <deahong1214@gmail.com>
2025-10-26 00:44:50 +0900

    치지직 태그 찾기 테스트

--- server/src/others/managed_data_domain.py
+++ server/src/others/managed_data_domain.py
@@ -412,6 +412,8 @@ class ManagedTable:
                     return str(search_value) == cell.strftime("%Y/%m/%d-%H:%M:%S")
 
             if isinstance(cell, list):
+                print("cell :", cell)
+                print("search_value :", search_value)
                 return any(str(search_value) == str(item) for item in cell)
 
             return str(search_value) == str(cell)
